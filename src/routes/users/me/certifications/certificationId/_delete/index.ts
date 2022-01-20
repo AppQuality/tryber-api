3bca370e6fe0fac5dcf15961c45160a778345270
Davide Bizzi <davide.bizzi@app-quality.com>
2022-01-20 15:45:28 +0100

    feat: Add certification delete

---
+++ src/routes/users/me/certifications/certificationId/_delete/index.ts
@@ -0,0 +1,46 @@
+/** OPENAPI-ROUTE: delete-users-me-certifications-certificationId */
+import { Context } from "openapi-backend";
+import * as db from "../../../../../../features/db";
+
+export default async (
+  c: Context,
+  req: OpenapiRequest,
+  res: OpenapiResponse
+) => {
+  try {
+    const certificationId = Array.isArray(c.request.params.certificationId)
+      ? c.request.params.certificationId.join(",")
+      : c.request.params.certificationId;
+    let existsCertQuery = `SELECT id FROM wp_appq_profile_certifications WHERE cert_id=? AND tester_id=?;`;
+    let existsCert = await db.query(
+      db.format(existsCertQuery, [certificationId, req.user.testerId])
+    );
+    if (!existsCert.length) {
+      throw {
+        status_code: 404,
+        message: "This tester has not this Certification.",
+      };
+    }
+
+    let deleteSql = `DELETE FROM wp_appq_profile_certifications WHERE cert_id=? AND tester_id=?;`;
+    let deleted;
+    try {
+      deleted = await db.query(
+        db.format(deleteSql, [certificationId, req.user.testerId])
+      );
+      res.status_code = 200;
+      return { message: "Certification successfully removed" };
+    } catch (e) {
+      if (process.env && process.env.DEBUG) console.log(e);
+      throw Error("Failed to remove user Certification");
+    }
+  } catch (error) {
+    if (process.env && process.env.DEBUG) console.log(error);
+    res.status_code = (error as OpenapiError).status_code || 400;
+    return {
+      element: "certifications",
+      id: parseInt(req.user.ID),
+      message: (error as OpenapiError).message,
+    };
+  }
+};
