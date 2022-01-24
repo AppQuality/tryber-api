14217a944d6e65f6a8d677141a61e4e718d0069a
Gianpaolo Sinatra <gianpaolo.sinatra@app-quality.com>
2022-01-24 18:21:07 +0100

    feat: put users me fiscal

---
+++ src/routes/users/me/fiscal/_put/index.ts
@@ -0,0 +1,97 @@
+/** OPENAPI-ROUTE:put-users-me-fiscal */
+import { Context } from 'openapi-backend';
+
+import * as db from '../../../../../features/db';
+import checkCodiceFiscale from '../checkCodiceFiscale';
+import getActiveProfile from '../getActiveProfile';
+import getByUser from '../getByUser';
+
+export default async (
+  c: Context,
+  req: OpenapiRequest,
+  res: OpenapiResponse
+) => {
+  try {
+    let fiscal = await getActiveProfile(req.user.testerId);
+
+    let tester = await db.query(
+      db.format(
+        `SELECT name,surname,COALESCE(CAST(birth_date as CHAR),"1970-01-01 00:00:00") as birth_date
+FROM wp_appq_evd_profile 
+WHERE id = ?`,
+        [req.user.testerId]
+      )
+    );
+    tester = tester[0];
+    if (!tester.birth_date) {
+      tester.birth_date = "";
+    }
+    const fiscalTypes = {
+      withholding: 1,
+      "witholding-extra": 2,
+      other: 3,
+      "non-italian": 4,
+    };
+
+    let isVerified = 1;
+    if (
+      [1, 2].includes(fiscalTypes[req.body.type as keyof typeof fiscalTypes])
+    ) {
+      isVerified = (await checkCodiceFiscale(req.body.fiscalId, {
+        name: tester.name,
+        surname: tester.surname,
+        gender: req.body.gender === "male" ? "M" : "F",
+        birthday: {
+          day: parseInt(tester.birth_date.substring(0, 10).split("-")[2]),
+          month: parseInt(tester.birth_date.substring(0, 10).split("-")[1]),
+          year: parseInt(tester.birth_date.substring(0, 10).split("-")[0]),
+        },
+        birthplace: req.body.birthPlace.city,
+        birthplaceProvincia: req.body.birthPlace.province,
+      }))
+        ? 1
+        : 0;
+    }
+    const updateSql = `UPDATE wp_appq_fiscal_profile 
+SET is_active = 0 WHERE id = ?`;
+    await db.query(db.format(updateSql, fiscal.id));
+
+    const updateData = {
+      is_active: 1,
+      name: tester.name,
+      surname: tester.surname,
+      birth_date: tester.birth_date,
+      tester_id: req.user.testerId,
+      country: req.body.address.country,
+      province: req.body.address.province,
+      city: req.body.address.city,
+      address: req.body.address.street,
+      address_number: req.body.address.streetNumber,
+      postal_code: req.body.address.cityCode,
+      fiscal_category: fiscalTypes[req.body.type as keyof typeof fiscalTypes],
+      birth_city: req.body.birthPlace.city,
+      birth_province: req.body.birthPlace.province,
+      fiscal_id: req.body.fiscalId,
+      sex: req.body.gender == "female" ? 0 : 1,
+      is_verified: isVerified,
+    };
+
+    const insertSql = `INSERT INTO wp_appq_fiscal_profile 
+      (${Object.keys(updateData).join(",")}) 
+      VALUES (${Object.keys(updateData)
+        .map((a) => "?")
+        .join(",")})`;
+
+    await db.query(db.format(insertSql, Object.values(updateData)));
+
+    res.status_code = 200;
+    return getByUser(req.user.testerId);
+  } catch (error) {
+    res.status_code = (error as OpenapiError).status_code || 500;
+    return {
+      element: "users",
+      id: parseInt(req.user.ID),
+      message: (error as OpenapiError).message,
+    };
+  }
+};
---
+++ src/routes/users/me/fiscal/checkCodiceFiscale.ts
@@ -0,0 +1,28 @@
+var CodiceFiscale = require("codice-fiscale-js");
+
+export default async (
+  fiscalCode:string,
+  { name, surname, gender, birthday, birthplace, birthplaceProvincia }: 
+  { name:string, surname:string, gender:string, birthday:{day:number,month:number,year:number}, birthplace:string, birthplaceProvincia:string }
+) => {
+  let cf;
+  let checkCf;
+  if (birthplaceProvincia === "EE") return true;
+  try {
+    cf = new CodiceFiscale(fiscalCode);
+    checkCf = new CodiceFiscale({
+      name,
+      surname,
+      gender,
+      day: birthday.day,
+      month: birthday.month,
+      year: birthday.year,
+      birthplace: birthplace,
+      birthplaceProvincia: birthplaceProvincia,
+    });
+    checkCf = new CodiceFiscale(checkCf.code);
+    return cf.code === checkCf.code;
+  } catch (e) {
+    return false;
+  }
+};
