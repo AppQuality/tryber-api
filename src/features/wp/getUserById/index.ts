9f49e9836fa8a4c4bb7e3d28bd2e63fa736e17dd
Davide Bizzi <davide.bizzi@app-quality.com>
2022-01-25 17:11:33 +0100

    fix: Fix ambiguous clause

--- src/features/wp/getUserById/index.ts
+++ src/features/wp/getUserById/index.ts
@@ -4,7 +4,7 @@ export default async (userId: number) => {
   const sql = `SELECT p.id as testerId, wp.ID,wp.user_login,wp.user_pass
   FROM wp_users wp
   JOIN wp_appq_evd_profile p ON wp.ID = p.wp_user_id 
-    WHERE ID = ?`;
+    WHERE wp.ID = ?`;
   try {
     const results = await db.query(db.format(sql, [userId]));
     return results[0];
