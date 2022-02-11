6883d6c7cc069ad5c22044a9c328f9a3c889c356
Davide Bizzi <davide.bizzi@app-quality.com>
2022-02-11 14:11:57 +0100

    test: Test paypal and transferwise function wrappers

---
+++ src/routes/payments/paymentId/_post/sendTransferwisePayment/index.spec.ts
@@ -0,0 +1,51 @@
+import Transferwise from "@src/features/tranferwise";
+
+import sendTransferwisePayment from ".";
+
+const paymentMockData: Payment = {
+  id: 1,
+  tester_id: 1,
+  accountName: "John Doe",
+  type: "paypal",
+  status: "pending",
+  coordinates: "DE12345678901234567890",
+  amount: 1,
+};
+jest.mock("@src/features/tranferwise");
+
+const mockFee = 0.35;
+
+describe("Send transferwise payment", () => {
+  it("Should return fee and update status as paid on success", async () => {
+    (Transferwise.prototype.createPayment as jest.Mock).mockImplementation(
+      () => ({
+        quote: {
+          paymentOptions: {
+            fee: { total: mockFee },
+          },
+        },
+      })
+    );
+
+    const data = await sendTransferwisePayment(paymentMockData);
+    expect(data).toMatchObject({
+      ...paymentMockData,
+      status: "paid",
+      fee: mockFee,
+    });
+  });
+  it("Should throw error on error", async () => {
+    (Transferwise.prototype.createPayment as jest.Mock).mockImplementation(
+      () => {
+        throw new Error("Transferwise error");
+      }
+    );
+
+    try {
+      const data = await sendTransferwisePayment(paymentMockData);
+      fail("Should throw error");
+    } catch (error) {
+      expect((error as OpenapiError).message).toBe("Transferwise error");
+    }
+  });
+});
