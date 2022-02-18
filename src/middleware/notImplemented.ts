4da033c11bd96db135b4dd09b22cbd4696aae1f2
Davide Bizzi <davide.bizzi@app-quality.com>
2022-02-18 19:45:45 +0100

    feat: Allow getting examples from any endpoint

--- src/middleware/notImplemented.ts
+++ src/middleware/notImplemented.ts
@@ -1,5 +1,4 @@
 import OpenAPIBackend, { Context } from "openapi-backend";
-import { OpenAPIV3 } from "openapi-types";
 
 export default (api: OpenAPIBackend) =>
   async (c: Context, req: Request, res: OpenapiResponse) => {
@@ -8,23 +7,8 @@ export default (api: OpenAPIBackend) =>
       console.log(`Mocking ${c.operation.operationId}`);
     }
 
-    let { status, mock } = api.mockResponseForOperation(
+    const { status, mock } = api.mockResponseForOperation(
       c.operation.operationId || ""
     );
-    const responses = c.operation.responses;
-    if (
-      Object.keys(req.headers).includes("x-tryber-mock-example") &&
-      responses
-    ) {
-      //@ts-ignore
-      const exampleName = req.headers["x-tryber-mock-example"] || "";
-      const response = responses[status] as OpenAPIV3.ResponseObject;
-      if (response && response.content) {
-        const examples = response.content["application/json"].examples;
-        if (examples && Object.keys(examples).includes(exampleName)) {
-          mock = examples[exampleName];
-        }
-      }
-    }
     return res.status(status).json(mock);
   };
