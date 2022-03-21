d96f5794ba801e2078c281a9228cb5c05c7ba395
Gianpaolo Sinatra <gianpaolo.sinatra@app-quality.com>
2022-03-21 12:55:03 +0100

    fix: remove useless dbFormat

--- src/features/wp/getCrowdOption/index.ts
+++ src/features/wp/getCrowdOption/index.ts
@@ -3,7 +3,7 @@ import { unserialize } from "php-unserialize";
 
 export default async (key: string) => {
   const sql = `SELECT option_value FROM wp_options WHERE option_name = "crowd_options_option_name"`;
-  const results = await db.query(db.format(sql, [key]));
+  const results = await db.query(sql);
   if (!results.length) {
     throw new Error(`Option crowd_options_option_name not found`);
   }
