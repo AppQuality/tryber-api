140d84cc1a032fd7f0114c819150e98169750ab1
Davide Bizzi <davide.bizzi@app-quality.com>
2022-03-22 15:46:13 +0100

    test(payments): Add iban payement test cases

--- src/routes/users/me/payments/_post/index.spec.ts
+++ src/routes/users/me/payments/_post/index.spec.ts
@@ -146,6 +146,132 @@ describe("POST /users/me/payments - valid paypal", () => {
   });
 });
 
+describe("POST /users/me/payments - valid iban", () => {
+  const data: any = {};
+  beforeEach(async () => {
+    return new Promise(async (resolve) => {
+      await profileTable.create();
+      await fiscalProfileTable.create();
+      await wpOptionsTable.create();
+      await wpOptionsData.crowdWpOptions();
+      await paymentRequestTable.create();
+
+      data.tester = await profileData.testerWithBooty({
+        pending_booty: 129.99,
+      });
+      data.fiscalProfile = await fiscalProfileData.validFiscalProfile({
+        tester_id: data.tester.id,
+      });
+      resolve(null);
+    });
+  });
+  afterEach(async () => {
+    return new Promise(async (resolve) => {
+      await profileTable.drop();
+      await fiscalProfileTable.drop();
+      await wpOptionsTable.drop();
+      await paymentRequestTable.drop();
+      resolve(null);
+    });
+  });
+
+  it("Should answer 200 if not logged in", async () => {
+    const response = await request(app)
+      .post("/users/me/payments")
+      .send({
+        method: {
+          type: "iban",
+          iban: "IT75T0300203280284975661141",
+          accountHolderName: "John Doe",
+        },
+      })
+      .set("Authorization", "Bearer tester");
+    expect(response.status).toBe(200);
+  });
+
+  it("Should create a row in the requests with the amount equal to current pending booty and is_paid=0", async () => {
+    const response = await request(app)
+      .post("/users/me/payments")
+      .send({
+        method: {
+          type: "iban",
+          iban: "IT75T0300203280284975661141",
+          accountHolderName: "John Doe",
+        },
+      })
+      .set("Authorization", "Bearer tester");
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty("id");
+    const requestId: number = response.body.id;
+    const requestData = await sqlite3.get(
+      `SELECT amount,is_paid FROM wp_appq_payment_request WHERE id=${requestId}`
+    );
+    expect(requestData.amount).toBe(data.tester.pending_booty);
+    expect(requestData.is_paid).toBe(0);
+  });
+  it("Should create a row in the requests with tester_id = current tester id", async () => {
+    const response = await request(app)
+      .post("/users/me/payments")
+      .send({
+        method: {
+          type: "iban",
+          iban: "IT75T0300203280284975661141",
+          accountHolderName: "John Doe",
+        },
+      })
+      .set("Authorization", "Bearer tester");
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty("id");
+    const requestId: number = response.body.id;
+    const requestData = await sqlite3.get(
+      `SELECT tester_id FROM wp_appq_payment_request WHERE id=${requestId}`
+    );
+    expect(requestData.tester_id).toBe(data.tester.id);
+  });
+
+  it("Should create a row in the requests with fiscal_profile_id = current tester fiscal id", async () => {
+    const response = await request(app)
+      .post("/users/me/payments")
+      .send({
+        method: {
+          type: "iban",
+          iban: "IT75T0300203280284975661141",
+          accountHolderName: "John Doe",
+        },
+      })
+      .set("Authorization", "Bearer tester");
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty("id");
+    const requestId: number = response.body.id;
+    const requestData = await sqlite3.get(
+      `SELECT fiscal_profile_id FROM wp_appq_payment_request WHERE id=${requestId}`
+    );
+    expect(requestData.fiscal_profile_id).toBe(data.fiscalProfile.id);
+  });
+
+  it("Should create a row in the requests with the body data for iban and account_holder and paypal_email null", async () => {
+    const response = await request(app)
+      .post("/users/me/payments")
+      .send({
+        method: {
+          type: "iban",
+          iban: "IT75T0300203280284975661141",
+          accountHolderName: "John Doe",
+        },
+      })
+      .set("Authorization", "Bearer tester");
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty("id");
+    const requestId: number = response.body.id;
+    const requestData = await sqlite3.get(
+      `SELECT paypal_email,iban,account_holder FROM wp_appq_payment_request WHERE id=${requestId}`
+    );
+    expect(requestData.paypal_email).toBe(null);
+    expect(requestData.iban).toBe("IT75T0300203280284975661141");
+    expect(requestData.account_holder).toBe("John Doe");
+  });
+});
+
 describe("POST /users/me/payments/ - fiscal profiles", () => {
   const data: any = {};
   beforeEach(async () => {
