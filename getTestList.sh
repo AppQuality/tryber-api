f5a0b0e5c72e5455664642d7623394d53ccce8ca
Davide Bizzi <davide.bizzi@app-quality.com>
2022-03-24 12:27:55 +0100

    chore: Allow test list extractor to filter by test

--- getTestList.sh
+++ getTestList.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-npm run -s test:ls | jq '.testResults[] | .assertionResults[] | select(.ancestorTitles[0] | startswith("'"$1"'")) | .fullName'
+PARAMS=""
+FILTERS=""
+if [[ ! -z $1 ]]; then
+	PARAMS="-- -t $1"
+	FILTERS='| startswith("'"$1"'")'
+fi
+
+npm run -s test:ls $PARAMS | jq '.testResults[] | .assertionResults[] | select(.ancestorTitles[0] '"$FILTERS"' ) | .fullName'
