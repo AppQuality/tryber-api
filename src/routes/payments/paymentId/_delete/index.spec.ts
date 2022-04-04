e695c5fdc0189a28d1db58eddea6998bf2629c1c
iacopolea <iacopo.lea@gmail.com>
2022-04-04 18:05:48 +0200

    feat(payment): delete 404 and 200

--- src/routes/payments/paymentId/_delete/index.spec.ts
+++ src/routes/payments/paymentId/_delete/index.spec.ts
@@ -1,22 +1,39 @@
 import app from "@src/app";
 import request from "supertest";
+import { table, data } from "@src/__mocks__/mockedDb/paymentRequest";
 
 jest.mock("@src/features/db");
 jest.mock("@appquality/wp-auth");
-
 describe("DELETE/payments/{paymentId}", () => {
+  beforeAll(async () => {
+    return new Promise(async (resolve) => {
+      await table.create();
+      await data.processingPaypalPayment();
+      resolve(null);
+    });
+  });
+  afterAll(async () => {
+    return new Promise(async (resolve) => {
+      await table.drop();
+      resolve(null);
+    });
+  });
   it("should return 403 if user is not admin", async () => {
     const response = await request(app)
       .delete("/payments/1")
       .set("authorization", "Bearer tester");
     expect(response.status).toBe(403);
   });
-  it("should return 200 if user is admin", async () => {
+  it("should return 200 if user is admin and paymentId exist", async () => {
     const response = await request(app)
       .delete("/payments/1")
       .set("authorization", "Bearer admin");
     expect(response.status).toBe(200);
   });
-  // it('should return 200 if paymentId exist')
-  // it('should return 404 if paymentId is not found')
+  it("should return 404 if user is admin and paymentId does not exist", async () => {
+    const response = await request(app)
+      .delete("/payments/2")
+      .set("authorization", "Bearer admin");
+    expect(response.status).toBe(404);
+  });
 });
--- src/routes/payments/paymentId/_delete/index.ts
+++ src/routes/payments/paymentId/_delete/index.ts
@@ -1,5 +1,6 @@
+import * as db from "@src/features/db";
+import debugMessage from "@src/features/debugMessage";
 import { Context } from "openapi-backend";
-
 /** OPENAPI-ROUTE: delete-payments-paymentId */
 export default async (
   c: Context,
@@ -10,7 +11,30 @@ export default async (
     res.status_code = 403;
     return {};
   }
-
-  res.status_code = 200;
-  return {};
+  let paymentId;
+  try {
+    paymentId = c.request.params.paymentId;
+    if (typeof paymentId !== "string") {
+      throw Error("Invalid payment query parameter");
+    }
+    let query = db.format(`DELETE FROM wp_appq_payment_request WHERE id = ?`, [
+      paymentId,
+    ]);
+    const result = await db.query(query);
+    if (result.changes === 1) {
+      res.status_code = 200;
+      return {};
+    } else {
+      res.status_code = 404;
+      return {
+        id: 0,
+        message: "request id not found",
+        element: "payment-request",
+      };
+    }
+  } catch (err) {
+    debugMessage(err);
+    res.status_code = 504;
+    return {};
+  }
 };
