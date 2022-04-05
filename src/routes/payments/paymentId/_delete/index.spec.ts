2a991d286cb8a9fd487c1924422f7ef99b8f4019
Gianpaolo Sinatra <gianpaolo.sinatra@app-quality.com>
2022-04-05 16:49:03 +0200

    feat(delete-payment-request): should return 403 if  request is paid

--- src/routes/payments/paymentId/_delete/index.spec.ts
+++ src/routes/payments/paymentId/_delete/index.spec.ts
@@ -1,38 +1,68 @@
+import {
+  data as paymentRequestData,
+  table as paymentRequestTable,
+} from "@src/__mocks__/mockedDb/paymentRequest";
 import app from "@src/app";
+import sqlite3 from "@src/features/sqlite";
 import request from "supertest";
-import { table, data } from "@src/__mocks__/mockedDb/paymentRequest";
 
 jest.mock("@src/features/db");
 jest.mock("@appquality/wp-auth");
-describe("DELETE/payments/{paymentId}", () => {
-  beforeAll(async () => {
+describe("DELETE /payments/{paymentId}", () => {
+  beforeEach(async () => {
     return new Promise(async (resolve) => {
-      await table.create();
-      await data.processingPaypalPayment();
+      await paymentRequestTable.create();
+      await paymentRequestData.processingPaypalPayment({ id: 1 });
+      await paymentRequestData.paidPaypalPayment({ id: 2 });
       resolve(null);
     });
   });
-  afterAll(async () => {
+  afterEach(async () => {
     return new Promise(async (resolve) => {
-      await table.drop();
+      await paymentRequestTable.drop();
       resolve(null);
     });
   });
-  it("should return 403 if user is not admin", async () => {
+  it("Should return 403 if user is not admin", async () => {
     const response = await request(app)
       .delete("/payments/1")
       .set("authorization", "Bearer tester");
     expect(response.status).toBe(403);
   });
-  it("should return 200 if user is admin and paymentId exist", async () => {
+  it("Should return 200 if user is admin and paymentId exist", async () => {
     const response = await request(app)
       .delete("/payments/1")
       .set("authorization", "Bearer admin");
     expect(response.status).toBe(200);
   });
-  it("should return 404 if user is admin and paymentId does not exist", async () => {
+  it("Should remove from database the payment_request if paymentId exists", async () => {
     const response = await request(app)
-      .delete("/payments/2")
+      .delete("/payments/1")
+      .set("authorization", "Bearer admin");
+    const resAfterDeletion = await sqlite3.get(
+      `SELECT * FROM wp_appq_payment_request WHERE id = 1`
+    );
+    expect(resAfterDeletion).toBe(undefined);
+  });
+  it("Should return 403 if payment_request is already paid", async () => {
+    const resBeforeDeletion = await sqlite3.all(
+      `SELECT * FROM wp_appq_payment_request`
+    );
+
+    const response = await request(app)
+      .delete("/payments/2") //2 is the id of the payment_request that is already paid
+      .set("authorization", "Bearer admin");
+    expect(response.status).toBe(403);
+
+    const resAfterDeletion = await sqlite3.all(
+      `SELECT * FROM wp_appq_payment_request`
+    );
+    expect(resAfterDeletion.length).toBe(resBeforeDeletion.length);
+  });
+
+  it("Should return 404 if user is admin and paymentId does not exist", async () => {
+    const response = await request(app)
+      .delete("/payments/69")
       .set("authorization", "Bearer admin");
     expect(response.status).toBe(404);
   });
--- src/routes/payments/paymentId/_delete/index.ts
+++ src/routes/payments/paymentId/_delete/index.ts
@@ -1,6 +1,7 @@
 import * as db from "@src/features/db";
 import debugMessage from "@src/features/debugMessage";
 import { Context } from "openapi-backend";
+
 /** OPENAPI-ROUTE: delete-payments-paymentId */
 export default async (
   c: Context,
@@ -17,9 +18,23 @@ export default async (
     if (typeof paymentId !== "string") {
       throw Error("Invalid payment query parameter");
     }
-    let query = db.format(`DELETE FROM wp_appq_payment_request WHERE id = ?`, [
-      paymentId,
-    ]);
+    let resCheckIsPaid = await db.query(
+      db.format(`SELECT is_paid FROM wp_appq_payment_request WHERE id = ? `, [
+        paymentId,
+      ])
+    );
+    const isPaid = resCheckIsPaid[0]?.is_paid ?? false;
+    if (isPaid) {
+      res.status_code = 403;
+      return {
+        message:
+          "the payment request is paid, you are not authorized to delete it",
+      };
+    }
+    let query = db.format(
+      `DELETE FROM wp_appq_payment_request WHERE id = ? AND is_paid = 0 `,
+      [paymentId]
+    );
     const result = await db.query(query);
     if (result.changes === 1) {
       res.status_code = 200;
@@ -34,7 +49,11 @@ export default async (
     }
   } catch (err) {
     debugMessage(err);
-    res.status_code = 504;
-    return {};
+    res.status_code = 404;
+    return {
+      id: 0,
+      message: "request id not found",
+      element: "payment-request",
+    };
   }
 };
