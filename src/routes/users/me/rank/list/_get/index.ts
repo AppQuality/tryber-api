e4e5c53bed9bb057c0871b00785f0be00e7e2f96
Davide Bizzi <davide.bizzi@app-quality.com>
2022-05-02 11:04:59 +0200

    rework(ranklist): Refactor getpeers

--- src/routes/users/me/rank/list/_get/index.ts
+++ src/routes/users/me/rank/list/_get/index.ts
@@ -2,6 +2,26 @@
 import Leaderboard from "@src/features/leaderboard";
 import { Context } from "openapi-backend";
 
+function getPeers(
+  leaderboard: StoplightComponents["schemas"]["RankingItem"][],
+  position: number
+) {
+  if ([1, 2, 3, 4].includes(position)) {
+    return leaderboard.slice(0, 9);
+  }
+  if (
+    [
+      leaderboard.length,
+      leaderboard.length - 1,
+      leaderboard.length - 2,
+      leaderboard.length - 3,
+    ].includes(position)
+  ) {
+    return leaderboard.slice(-9);
+  }
+  return leaderboard.slice(position - 4 - 1, position + 4);
+}
+
 export default async (
   c: Context,
   req: OpenapiRequest,
@@ -17,25 +37,10 @@ export default async (
       message: "You are not ranked",
     };
   }
-  let peers: StoplightComponents["schemas"]["RankingItem"][] = [];
-  if ([1, 2, 3, 4].includes(myRank.position)) {
-    peers = result.slice(0, 9);
-  } else if (
-    [
-      result.length,
-      result.length - 1,
-      result.length - 2,
-      result.length - 3,
-    ].includes(myRank.position)
-  ) {
-    peers = result.slice(-9);
-  } else {
-    peers = result.slice(myRank.position - 4 - 1, myRank.position + 4);
-  }
 
   res.status_code = 200;
   return {
     tops: result.slice(0, 3),
-    peers: peers,
+    peers: getPeers(result, myRank.position),
   };
 };
