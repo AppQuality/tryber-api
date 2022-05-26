7c4f4ca537ded38111ddf94308b70717ffcd6170
Davide Bizzi <davide.bizzi@app-quality.com>
2022-05-26 15:06:21 +0200

    fix: Remove useless code

--- src/routes/users/me/bugs/media/_post/index.ts
+++ src/routes/users/me/bugs/media/_post/index.ts
@@ -7,38 +7,10 @@ export default async (
   res: OpenapiResponse
 ) => {
   try {
-    console.log("REQ");
-    console.log(req.body);
-
-    //   var aws = require('aws-sdk')
-    // var express = require('express')
-    // var multer = require('multer')
-    // var multerS3 = require('multer-s3')
-
-    // var app = express()
-    // var s3 = new aws.S3({ /* ... */ })
-
-    // var upload = multer({
-    //   storage: multerS3({
-    //     s3: s3,
-    //     bucket: 'some-bucket',
-    //     metadata: function (req, file, cb) {
-    //       cb(null, {fieldName: file.fieldname});
-    //     },
-    //     key: function (req, file, cb) {
-    //       cb(null, Date.now().toString())
-    //     }
-    //   })
-    // })
-
-    app.post("/upload", upload.array("photos", 3), function (req, res, next) {
-      res.send("Successfully uploaded " + req.files.length + " files!");
-    });
-
-    const results = {};
-    console.log(results);
+    //@ts-ignore
+    console.log(req.files);
     res.status_code = 200;
-    return results;
+    return {};
   } catch (error) {
     if (process.env && process.env.DEBUG) console.log(error);
     res.status_code = 404;
