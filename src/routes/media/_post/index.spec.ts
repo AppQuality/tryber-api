38c369a0edd5a9388ff98e88ec387341d1caffb0
Davide Bizzi <davide.bizzi@app-quality.com>
2022-05-27 16:52:22 +0200

    fix(media): Mock s3 upload

--- src/routes/media/_post/index.spec.ts
+++ src/routes/media/_post/index.spec.ts
@@ -1,20 +1,27 @@
+import app from "@src/app";
+import upload from "@src/features/upload";
 import { data as profileData } from "@src/__mocks__/mockedDb/profile";
 import { data as wpUserData } from "@src/__mocks__/mockedDb/wp_users";
-import app from "@src/app";
 import request from "supertest";
 
 jest.mock("@src/features/db");
 jest.mock("@appquality/wp-auth");
+jest.mock("@src/features/upload");
 
 describe("Route POST /media", () => {
-  beforeEach(async () => {
+  beforeAll(async () => {
+    (upload as jest.Mock).mockImplementation(
+      ({ key, bucket }: { bucket: string; key: string }) => {
+        return `https://s3.amazonaws.com/${bucket}/${key}`;
+      }
+    );
     return new Promise(async (resolve) => {
       await profileData.basicTester();
       await wpUserData.basicUser();
       resolve(null);
     });
   });
-  afterEach(async () => {
+  afterAll(async () => {
     return new Promise(async (resolve, reject) => {
       try {
         await wpUserData.drop();
@@ -29,11 +36,18 @@ describe("Route POST /media", () => {
     const response = await request(app).post("/media");
     expect(response.status).toBe(403);
   });
-  it("Should answer 404 if try to send file as .bat, .sh and .exe", async () => {
+  it("Should answer 200 if try to send file as .bat, .sh and .exe", async () => {
     const response = await request(app)
       .post("/media")
       .attach("media", "./src/__mocks__/exampleFiles/void.bat")
+      .attach("media", "./src/__mocks__/exampleFiles/image.png")
+      .attach("media", "./src/__mocks__/exampleFiles/void.sh")
       .set("authorization", "Bearer tester");
-    expect(response.status).toBe(404);
+    expect(response.status).toBe(200);
+    console.log(response.body);
+    expect(response.body).toHaveProperty("failed", [
+      { name: "void.bat" },
+      { name: "void.sh" },
+    ]);
   });
 });
