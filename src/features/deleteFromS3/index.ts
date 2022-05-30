269b288c9d5f15834ce473f175b6835a6e13290c
Davide Bizzi <davide.bizzi@app-quality.com>
2022-05-30 17:26:43 +0200

    fix(media): Remove async on delete

--- src/features/deleteFromS3/index.ts
+++ src/features/deleteFromS3/index.ts
@@ -7,14 +7,14 @@ const decomposeS3Url = (url: string): { bucket: string; key: string } => {
   return { bucket, key };
 };
 
-export default async ({ url }: { url: string }) => {
+export default ({ url }: { url: string }) => {
   const { bucket, key } = decomposeS3Url(url);
   const awsCredentials = {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   };
   const s3 = new S3(awsCredentials);
-  return await s3.deleteObject({ Bucket: bucket, Key: key }).promise();
+  return s3.deleteObject({ Bucket: bucket, Key: key }).promise();
 };
 
 export { decomposeS3Url };
