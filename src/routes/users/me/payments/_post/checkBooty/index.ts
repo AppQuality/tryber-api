1a3c9ba2631f4862f3f9b5e5c645016c1be19b9b
Gianpaolo Sinatra <gianpaolo.sinatra@app-quality.com>
2022-06-10 15:22:10 +0200

    feat(users/me/payments post): refactor check-booty-treshold

--- src/routes/users/me/payments/_post/checkBooty/index.ts
+++ src/routes/users/me/payments/_post/checkBooty/index.ts
@@ -2,21 +2,23 @@ import * as db from "@src/features/db";
 import getCrowdOption from "@src/features/wp/getCrowdOption";
 
 export default async (testerId: number) => {
-  const currentBooty = await db.query(
+  const attributions = await db.query(
     db.format(
       `
-  SELECT pending_booty 
-  FROM wp_appq_evd_profile
-  WHERE id = ?`,
+      SELECT sum(amount) as total
+      FROM wp_appq_payment
+      WHERE tester_id = ?
+        AND is_paid = 0
+        AND is_requested = 0`,
       [testerId]
     )
   );
-  if (currentBooty.length === 0 || currentBooty[0].pending_booty === 0) {
+  if (attributions.length === 0 || attributions[0].total === 0) {
     throw new Error("You don't have any booty to pay");
   }
   const threshold = await getCrowdOption("minimum_payout");
-  if (threshold && currentBooty[0].pending_booty < threshold) {
+  if (threshold && attributions[0].total < threshold) {
     throw new Error(`You need to have at least ${threshold} booty to pay`);
   }
-  return currentBooty[0].pending_booty;
+  return attributions[0].total;
 };
