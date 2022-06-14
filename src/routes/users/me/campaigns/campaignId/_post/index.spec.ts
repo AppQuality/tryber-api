34dc97aa874856230cf76592f8ae81e2f686c051
Gianpaolo Sinatra <gianpaolo.sinatra@app-quality.com>
2022-06-14 11:44:57 +0200

    feat(user post bug in a specific CP): Should answer 403 if user is not selected on CP

--- src/routes/users/me/campaigns/campaignId/_post/index.spec.ts
+++ src/routes/users/me/campaigns/campaignId/_post/index.spec.ts
@@ -1,11 +1,27 @@
 import { data as campaignData } from "@src/__mocks__/mockedDb/campaign";
+import { data as cpCandidaturesData } from "@src/__mocks__/mockedDb/cp_has_candidates";
 import app from "@src/app";
 import request from "supertest";
 
+const bug = {
+  title: "Camapign Title",
+  description: "Camapign Description",
+  expected: "The expected to reproduce the bug",
+  current: "Current case",
+  severity: "LOW",
+  replicability: "ONCE",
+  type: "CRASH",
+  notes: "The bug notes",
+  usecase: 1,
+  device: 0,
+  media: ["the media1 url"],
+};
 describe("Route POST a bug to a specific campaign", () => {
   beforeEach(async () => {
     return new Promise(async (resolve) => {
       await campaignData.basicCampaign();
+      await campaignData.basicCampaign({ id: 3 });
+      await cpCandidaturesData.candidate1();
 
       resolve(null);
     });
@@ -13,6 +29,7 @@ describe("Route POST a bug to a specific campaign", () => {
   afterEach(async () => {
     return new Promise(async (resolve) => {
       await campaignData.drop();
+      await cpCandidaturesData.drop();
 
       resolve(null);
     });
@@ -20,32 +37,30 @@ describe("Route POST a bug to a specific campaign", () => {
 
   it("Should answer 403 if not logged in", async () => {
     const response = await request(app).post("/users/me/campaigns/1/bugs");
-    console.log(response.body);
     expect(response.status).toBe(403);
   });
   it("Should answer 404 if campaign does not exist", async () => {
     const response = await request(app)
       .post("/users/me/campaigns/2/bugs")
       .set("authorization", "Bearer tester")
-      .send({
-        title: "Camapign Title",
-        description: "Camapign Description",
-        expected: "The expected to reproduce the bug",
-        current: "Current case",
-        severity: "LOW",
-        replicability: "ONCE",
-        type: "CRASH",
-        notes: "The bug notes",
-        usecase: 1,
-        device: 0,
-        media: ["the media1 url"],
-      });
-    console.log(response.body);
+      .send(bug);
     expect(response.status).toBe(404);
     expect(response.body).toEqual({
       element: "post a bug",
       id: 0,
       message: "CampaignId 2, does not exists.",
+    });
+  });
+  it("Should answer 403 if user is not selected on CP", async () => {
+    const response = await request(app)
+      .post("/users/me/campaigns/3/bugs")
+      .set("authorization", "Bearer tester")
+      .send(bug);
+    expect(response.status).toBe(403);
+    expect(response.body).toEqual({
+      element: "post a bug",
+      id: 0,
+      message: "T1 is not candidate on CP3.",
     });
   });
 });
