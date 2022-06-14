20a5ac6c92be03b0a2e0fc8391d05246955b77b6
Gianpaolo Sinatra <gianpaolo.sinatra@app-quality.com>
2022-06-14 11:09:53 +0200

    feat(user post bug in a specific CP): Should answer 404 if campaign does not exist

--- src/routes/users/me/campaigns/campaignId/_post/index.spec.ts
+++ src/routes/users/me/campaigns/campaignId/_post/index.spec.ts
@@ -23,4 +23,29 @@ describe("Route POST a bug to a specific campaign", () => {
     console.log(response.body);
     expect(response.status).toBe(403);
   });
+  it("Should answer 404 if campaign does not exist", async () => {
+    const response = await request(app)
+      .post("/users/me/campaigns/2/bugs")
+      .set("authorization", "Bearer tester")
+      .send({
+        title: "Camapign Title",
+        description: "Camapign Description",
+        expected: "The expected to reproduce the bug",
+        current: "Current case",
+        severity: "LOW",
+        replicability: "ONCE",
+        type: "CRASH",
+        notes: "The bug notes",
+        usecase: 1,
+        device: 0,
+        media: ["the media1 url"],
+      });
+    console.log(response.body);
+    expect(response.status).toBe(404);
+    expect(response.body).toEqual({
+      element: "post a bug",
+      id: 0,
+      message: "CampaignId 2, does not exists.",
+    });
+  });
 });
