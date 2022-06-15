58699d6cecfb4ddd99ebe35a4043e4eb193eb87f
Gianpaolo Sinatra <gianpaolo.sinatra@app-quality.com>
2022-06-15 11:39:50 +0200

    chore: rename mocked-tables-files and move index and spec to right place

--- src/routes/campaigns/campaignId/candidates/_post/index.spec.ts
+++ src/routes/campaigns/campaignId/candidates/_post/index.spec.ts
@@ -4,6 +4,8 @@ import Campaigns from "@src/__mocks__/mockedDb/campaign";
 import Candidature from "@src/__mocks__/mockedDb/cp_has_candidates";
 import { data as testerData } from "@src/__mocks__/mockedDb/profile";
 import { data as wpUsersData } from "@src/__mocks__/mockedDb/wp_users";
+import app from "@src/app";
+import sqlite3 from "@src/features/sqlite";
 import request from "supertest";
 
 describe("POST /campaigns/{campaignId}/candidates", () => {
