b0a22904460a6e3f93f957119a5f47a43d5756a2
Gianpaolo Sinatra <gianpaolo.sinatra@app-quality.com>
2022-06-17 17:47:23 +0200

    wip: test post users/me/campigns/CPID/bugs

--- src/__mocks__/mockedDb/bug.ts
+++ src/__mocks__/mockedDb/bug.ts
@@ -4,12 +4,34 @@ const tableName = "wp_appq_evd_bug";
 export const table = {
   create: async () => {
     await sqlite3.createTable(tableName, [
-      "id INTEGER PRIMARY KEY",
+      "id INTEGER PRIMARY KEY NOT NULL",
+      "internal_id VARCHAR(45)",
+      "wp_user_id INTEGER(11) NOT NULL",
       "message VARCHAR(255)",
+      "description VARCHAR(255)",
+      "expected_result VARCHAR(255)",
+      "current_result VARCHAR(255)",
       "campaign_id INTEGER",
       "status_id INTEGER",
-      "wp_user_id INTEGER",
-      "severity_id INTEGER",
+      "severity_id INTEGER DEFAULT 1",
+      "publish INTEGER DEFAULT 1",
+      "status_reason VARCHAR(255)",
+      "created DATETIME",
+      "updated DATETIME",
+      "bug_replicability_id INTEGER",
+      "bug_type_id INTEGER",
+      "application_section VARCHAR(255)",
+      "application_section_id INTEGER",
+      "note VARCHAR(3000)",
+      "dev_id INTEGER",
+      "manufacturer VARCHAR(65)",
+      "model VARCHAR(65)",
+      "os VARCHAR(45)",
+      "os_version VARCHAR(45)",
+      "version_id INTEGER",
+      "reviewer INTEGER NOT NULL",
+      "is_perfect INTEGER DEFAULT 1",
+      "last_editor_id INTEGER NOT NULL",
     ]);
   },
   drop: async () => {
@@ -19,11 +41,33 @@ export const table = {
 
 type BugParams = {
   id?: number;
+  internal_id?: string;
+  wp_user_id?: number;
   message?: string;
+  description?: string;
+  expected_result?: string;
+  current_result?: string;
   campaign_id?: number;
   status_id?: number;
-  wp_user_id?: number;
   severity_id?: number;
+  publish?: number;
+  status_reason?: string;
+  created?: string;
+  updated?: string;
+  bug_replicability_id?: number;
+  bug_type_id?: number;
+  application_section?: string;
+  application_section_id?: number;
+  note?: string;
+  dev_id?: number;
+  manufacturer?: string;
+  model?: string;
+  os?: string;
+  os_version?: string;
+  version_id?: number;
+  reviewer?: number;
+  is_perfect?: number;
+  last_editor_id?: number;
 };
 const data: {
   [key: string]: (params?: BugParams) => Promise<{ [key: string]: any }>;
@@ -37,6 +81,7 @@ data.basicBug = async (params) => {
   const item = {
     id: 1,
     wp_user_id: 1,
+    campaign_id: 1,
     ...params,
   };
   await sqlite3.insert(tableName, item);
--- src/routes/users/me/campaigns/campaignId/bugs/_post/index.ts
+++ src/routes/users/me/campaigns/campaignId/bugs/_post/index.ts
@@ -13,17 +13,38 @@ export default async (
   try {
     await campaignExists();
     const candidature = await getTesterCandidature();
-    if (await campaignHasTasks()) await useCaseIsValid(candidature.group_id);
+    const usecase = await useCaseIsValid(candidature.group_id);
     await severityIsAcceptable();
     await replicabilityIsAcceptable();
     await bugTypeIsAcceptable();
 
     res.status_code = 200;
-    return {}; //await createBug();
+    const insertedBug = await createBug(usecase);
+    await updateInternalBugId(insertedBug.id);
+    return {
+      id: insertedBug.id,
+      testerId: 1,
+      title: "Camapign Title",
+      description: "Camapign Description",
+      status: "PENDING",
+      expected: "The expected to reproduce the bug",
+      current: "Current case",
+      severity: "LOW",
+      replicability: "ONCE",
+      type: "CRASH",
+      notes: "The bug notes",
+      usecase: "1",
+      device: {
+        id: 0,
+        type: "DESKTOP",
+        device: { pc_type: "WINDOWS" },
+        operating_system: { id: 0, platform: "", version: "" },
+      },
+      media: ["the media1 url"],
+    };
   } catch (error) {
-    if (process.env && process.env.DEBUG)
-      //console.log(error);
-      res.status_code = (error as OpenapiError).status_code || 400;
+    if (process.env && process.env.DEBUG) console.log(error);
+    res.status_code = (error as OpenapiError).status_code || 400;
     return {
       element: "bugs",
       id: 0,
@@ -139,69 +160,209 @@ export default async (
       };
     }
   }
-  async function campaignHasTasks() {
-    let tasks = await db.query(
-      db.format(
-        `SELECT id
-          FROM wp_appq_campaign_task
-          WHERE campaign_id = ?
-         ;`,
-        [campaignId]
-      )
-    );
-    return tasks.length;
-  }
-  async function useCaseIsValid(group_id: number) {
-    let usecase = await db.query(
-      db.format(
-        `SELECT tsk.id AS id
+  async function useCaseIsValid(
+    group_id: number
+  ): Promise<{ id: number; title: string }> {
+    let usecase = { id: 0, title: "" };
+    if (isNotSpecificUsecase()) {
+      usecase = { id: -1, title: "Not a specific use case" };
+    } else {
+      let usecases = await db.query(
+        db.format(
+          `SELECT tsk.id AS id, tsk.title AS title
           FROM wp_appq_campaign_task tsk
                    JOIN wp_appq_campaign_task_group tskgrp ON tskgrp.task_id = tsk.id
           WHERE tsk.campaign_id = ?
             AND (tskgrp.group_id = 0 || tskgrp.group_id = ?)
             AND tsk.id = ?
          ;`,
-        [campaignId, group_id, req.body.usecase]
+          [campaignId, group_id, req.body.usecase]
+        )
+      );
+      if (!usecases.length) {
+        throw {
+          status_code: 403,
+          message: `Usecase ${req.body.usecase} not found for CP${campaignId}.`,
+        };
+      }
+      usecase = usecases[0];
+    }
+    return usecase;
+  }
+
+  function isNotSpecificUsecase() {
+    return req.body.usecase === -1;
+  }
+  async function getSeverityIdFromName(): Promise<number> {
+    const severity = await db.query(
+      db.format(
+        `SELECT id
+        FROM wp_appq_evd_severity
+        WHERE name = ?        
+        ;`,
+        [req.body.severity]
       )
     );
-    if (!usecase.length) {
+    if (!severity.length) {
       throw {
         status_code: 403,
-        message: `Usecase ${req.body.usecase} not found for CP${campaignId}.`,
+        message: `Severity ${req.body.severity} not found.`,
       };
     }
+    return severity[0].id;
   }
-  // async function createBug() {
-  //   let inserted = await db.query(
-  //     db.format(
-  //       `INSERT INTO table_name (message, campaign_id, status_id, wp_user_id, severity_id)
-  //       VALUES ('hola', ?, 1, ...);
-  //        ;`,
-  //       [campaignId]
-  //     )
-  //   );
-  //   if (inserted.affectedRows === 0) {
-  //     throw {
-  //       status_code: 403,
-  //       message: `Error on uploading Bug`,
-  //     };
-  //   }
-  //   return {
-  //     id: inserted.id,
-  //     testerId: req.user.testerId,
-  //     title: req.body.title,
-  //     status: req.body.status,
-  //     description: req.body.description,
-  //     expected: req.body.expected,
-  //     current: req.body.current,
-  //     severity: req.body.severity,
-  //     replicability: req.body.replicability,
-  //     type: req.body.type,
-  //     notes: req.body.notes,
-  //     usecase: req.body.usecase,
-  //     //device: ??
-  //     media: ["URL of media ?????"],
-  //     //additional: ??
-  //   };
-  // }
+  async function getReplicabilityIdFromName(): Promise<number> {
+    const replicability = await db.query(
+      db.format(
+        `SELECT id
+        FROM wp_appq_evd_bug_replicability
+        WHERE name = ?     
+        ;`,
+        [req.body.replicability]
+      )
+    );
+    if (!replicability.length) {
+      throw {
+        status_code: 403,
+        message: `Replicability ${req.body.replicability} not found.`,
+      };
+    }
+    return replicability[0].id;
+  }
+  async function getBugTypeIdFromName(): Promise<number> {
+    const type = await db.query(
+      db.format(
+        `SELECT id
+        FROM wp_appq_evd_bug_type
+        WHERE name = ?           
+        ;`,
+        [req.body.type]
+      )
+    );
+    if (!type.length) {
+      throw {
+        status_code: 403,
+        message: `Type ${req.body.type} not found.`,
+      };
+    }
+    return type[0].id;
+  }
+  async function createBug(usecase: { id: number; title: string }) {
+    const severityId = await getSeverityIdFromName();
+    const replicabilityId = await getReplicabilityIdFromName();
+    const bugTypeId = await getBugTypeIdFromName();
+    const device = await getUserDevice();
+    let inserted = await db.query(
+      db.format(
+        `INSERT INTO wp_appq_evd_bug (
+          wp_user_id,
+          message,
+          description,
+          expected_result,
+          current_result,
+          campaign_id,
+
+          status_id,
+          publish,
+          status_reason,
+
+          severity_id,
+
+          created,
+          updated,
+
+          bug_replicability_id,
+          bug_type_id,
+          application_section,
+          application_section_id,
+          note,
+          dev_id,
+          manufacturer,
+          model,
+          os,
+          os_version,
+
+          version_id,
+          reviewer,
+          is_perfect,
+          last_editor_id,
+        )
+        VALUES (
+          ?,?,?,?,?,?,
+          3,1,"Bug under review.",
+          ?,
+          NOW(),NULL,
+          ?,?,?,?,?,?,?,?,?,?,
+          -1,0,1,0,)
+         ;`,
+        [
+          req.user.ID,
+          req.body.title,
+          req.body.description,
+          req.body.expected,
+          req.body.current,
+          campaignId,
+          severityId,
+          replicabilityId,
+          bugTypeId,
+          usecase.title,
+          usecase.id,
+          req.body.note,
+          device.id,
+          device.manufacturer,
+          device.model,
+          device.os,
+          device.os_version,
+        ]
+      )
+    );
+    if (inserted.affectedRows === 0) {
+      throw {
+        status_code: 403,
+        message: `Error on uploading Bug`,
+      };
+    }
+    return {
+      id: inserted.id,
+      testerId: req.user.testerId,
+      title: req.body.title,
+      status: req.body.status,
+      description: req.body.description,
+      expected: req.body.expected,
+      current: req.body.current,
+      severity: req.body.severity,
+      replicability: req.body.replicability,
+      type: req.body.type,
+      notes: req.body.notes,
+      usecase: req.body.usecase,
+      //device: ??
+      media: req.body.media,
+      //additional: ??
+    };
+  }
+
+  async function updateInternalBugId(bugId: number) {
+    const internalBugId = (
+      await db.query(
+        db.format(
+          `SELECT CONCAT(cp.base_bug_internal_id, bug.id) AS internal
+          FROM wp_appq_evd_campaign cp
+                   JOIN wp_appq_evd_bug bug ON cp.id = bug.campaign_id
+          where bug.id = ?
+         ;`,
+          [bugId]
+        )
+      )
+    )[0].internal;
+
+    await db.query(
+      db.format(
+        `UPDATE wp_appq_evd_bug
+          SET internal_id = ?
+          WHERE id = ?          
+         ;`,
+        [internalBugId, bugId]
+      )
+    );
+  }
 };
