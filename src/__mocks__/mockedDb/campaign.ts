4cf69337df1a1ed20f32758d5c9a3691fd93decf
Davide Bizzi <davide.bizzi@app-quality.com>
2022-06-24 09:34:48 +0200

    feat(campaign): Add allowed replicability types and severity

--- src/__mocks__/mockedDb/campaign.ts
+++ src/__mocks__/mockedDb/campaign.ts
@@ -2,13 +2,24 @@ import Table from "./table";
 
 type CampaignParams = {
   id?: number;
+  title?: string;
+  min_allowed_media?: number;
+  campaign_type?: -1 | 0 | 1;
 };
 class Campaign extends Table<CampaignParams> {
   protected name = "wp_appq_evd_campaign";
-  protected columns = ["id INTEGER(11) PRIMARY KEY", "title VARCHAR(255)"];
+  protected columns = [
+    "id INTEGER(11) PRIMARY KEY",
+    "title VARCHAR(255)",
+    "min_allowed_media INTEGER(11)",
+    "campaign_type INTEGER(11)",
+  ];
   constructor() {
     super({
       id: 1,
+      title: "Test Campaign",
+      min_allowed_media: 1,
+      campaign_type: 0,
     });
   }
 }
