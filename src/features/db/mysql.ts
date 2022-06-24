78c957d1d6016efaefa0e2cc96c225db427caa74
Davide Bizzi <davide.bizzi@app-quality.com>
2022-06-24 12:05:11 +0200

    feat: Add round robin connections

--- src/features/db/mysql.ts
+++ src/features/db/mysql.ts
@@ -1,16 +1,22 @@
 import mysql, { Connection } from "mysql";
 import config from "../../config";
 
-var _connection: Connection;
+var _connection: Connection[] = [];
+var _connectionId: number = 0;
+var _maxConnection: number = parseInt(process.env.CONNECTION_COUNT || "1");
 
 export default {
   connectToServer: function (callback: () => void) {
-    _connection = mysql.createConnection(config.db);
-    _connection.connect();
+    for (let i = 0; i < _maxConnection; i++) {
+      _connection[i] = mysql.createConnection(config.db);
+      _connection[i].connect();
+    }
     return callback();
   },
 
   getConnection: function () {
-    return _connection;
+    console.log("#" + _connectionId);
+    _connectionId = (_connectionId + 1) % _maxConnection;
+    return _connection[_connectionId];
   },
 };
