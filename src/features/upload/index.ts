010602c4c1cdf3a640f0e66e319b4b4bf2305936
Davide Bizzi <davide.bizzi@app-quality.com>
2022-06-29 12:10:50 +0200

    feat: Add busboy

--- src/features/upload/index.ts
+++ src/features/upload/index.ts
@@ -1,10 +1,10 @@
 import { S3 } from "aws-sdk";
 import { UploadedFile } from "express-fileupload";
-
+import stream from "stream";
 type UploadParams = {
   bucket: string;
   key: string;
-  file: UploadedFile;
+  file: Media;
 };
 
 export default async ({ bucket, key, file }: UploadParams): Promise<string> => {
@@ -13,14 +13,18 @@ export default async ({ bucket, key, file }: UploadParams): Promise<string> => {
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   };
   const s3 = new S3(awsCredentials);
+  const pass = new stream.PassThrough();
 
-  const data = await s3
+  const promise = s3
     .upload({
       Bucket: bucket,
       Key: key,
-      Body: file.data,
+      Body: pass,
       ACL: "public-read",
     })
     .promise();
+  const pipeline = file.stream.pipe(pass);
+  const data = await promise;
+
   return data.Location; // returns the url location
 };
--- src/routes/media/_post/index.ts
+++ src/routes/media/_post/index.ts
@@ -2,6 +2,39 @@ import debugMessage from "@src/features/debugMessage";
 import upload from "@src/features/upload";
 import { Context } from "openapi-backend";
 import path from "path";
+import fs from "fs";
+
+const parseForm = async (req: OpenapiRequest): Promise<Media[]> => {
+  return new Promise((resolve, reject) => {
+    const form = req.busboy;
+    const files: Media[] = []; // create an empty array to hold the processed files
+    form.on("file", (field: any, readableStream: any, fileData: any) => {
+      const filePath = path.join("./tmp", fileData.filename);
+      const fstream = fs.createWriteStream(filePath);
+      const readStream = fs.createReadStream(filePath);
+
+      readableStream.on("data", (data: any) => {
+        fstream.write(data);
+      });
+      readableStream.on("end", () => {
+        files.push({
+          stream: readStream,
+          mimeType: fileData.mimeType,
+          name: fileData.filename,
+          size: fstream.bytesWritten,
+          tmpPath: filePath,
+        });
+      });
+    });
+    form.on("error", (err: any) => {
+      reject(err);
+    });
+    form.on("finish", () => {
+      resolve(files);
+    });
+    req.pipe(form); // pipe the request to the form handler
+  });
+};
 
 /** OPENAPI-ROUTE: post-media */
 export default async (
@@ -10,15 +43,13 @@ export default async (
   res: OpenapiResponse
 ) => {
   try {
-    if (!req.files.media) {
-      throw new Error("No file was uploaded");
+    if (!fs.existsSync("./tmp")) {
+      fs.mkdirSync("./tmp");
     }
-    const files = Array.isArray(req.files.media)
-      ? req.files.media
-      : [req.files.media];
-
+    const filesToUpload = await parseForm(req);
+    console.log(filesToUpload);
     res.status_code = 200;
-    return await uploadFiles(files, req.user.testerId);
+    return await uploadFiles(filesToUpload, req.user.testerId);
   } catch (err) {
     debugMessage(err);
     res.status_code = 404;
@@ -33,24 +64,22 @@ export default async (
     testerId,
     filename,
     extension,
-    folder,
   }: {
     testerId: number;
     filename: string;
     extension: string;
-    folder?: string;
   }): string {
     return `${
       process.env.MEDIA_FOLDER || "media"
     }/T${testerId}/${filename}_${new Date().getTime()}${extension}`;
   }
 
-  function isAcceptableFile(file: ApiUploadedFile): boolean {
+  function isAcceptableFile(file: { name: string }): boolean {
     return ![".bat", ".sh", ".exe"].includes(path.extname(file.name));
   }
 
   async function uploadFiles(
-    files: ApiUploadedFile[],
+    files: Media[],
     testerId: number
   ): Promise<
     StoplightOperations["post-media"]["responses"]["200"]["content"]["application/json"]
@@ -82,6 +111,7 @@ export default async (
           ).toString(),
         });
       }
+      fs.unlinkSync(media.tmpPath);
     }
     return {
       files: uploadedFiles,
@@ -89,9 +119,6 @@ export default async (
     };
   }
 };
-function isOversizedFile(media: ApiUploadedFile): boolean {
-  return (
-    typeof media.size !== "number" ||
-    media.size > parseInt(process.env.MAX_FILE_SIZE || "536870912")
-  );
+function isOversizedFile(media: Media): boolean {
+  return media.size > parseInt(process.env.MAX_FILE_SIZE || "536870912");
 }
