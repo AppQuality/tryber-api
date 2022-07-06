17fd08c7838ea72731ce7ead4fc82de6486e07e4
Davide Bizzi <davide.bizzi@app-quality.com>
2022-07-06 09:15:43 +0200

    feat: Prevent deletion of media if already linked to bug

--- src/routes/media/_delete/index.spec.ts
+++ src/routes/media/_delete/index.spec.ts
@@ -2,34 +2,29 @@ import app from "@src/app";
 import deleteFromS3 from "@src/features/deleteFromS3";
 import { data as profileData } from "@src/__mocks__/mockedDb/profile";
 import { data as wpUserData } from "@src/__mocks__/mockedDb/wp_users";
+import bugMedia from "@src/__mocks__/mockedDb/bugMedia";
 import request from "supertest";
 
 jest.mock("@src/features/deleteFromS3");
 
 process.env.MEDIA_BUCKET = "media.bucket";
 process.env.MEDIA_FOLDER = "media";
+
+beforeAll(async () => {
+  (deleteFromS3 as jest.Mock).mockImplementation(
+    ({ url }: { url: string }): Promise<any> => Promise.resolve(true)
+  );
+  await profileData.basicTester();
+  await wpUserData.basicUser();
+});
+
+afterAll(async () => {
+  await wpUserData.drop();
+  await profileData.drop();
+});
 describe("Route DELETE /media", () => {
-  beforeAll(async () => {
-    (deleteFromS3 as jest.Mock).mockImplementation(
-      ({ url }: { url: string }): Promise<any> => Promise.resolve(true)
-    );
-    return new Promise(async (resolve) => {
-      await profileData.basicTester();
-      await wpUserData.basicUser();
-      resolve(null);
-    });
-  });
-  afterAll(async () => {
-    return new Promise(async (resolve, reject) => {
-      try {
-        await wpUserData.drop();
-        await profileData.drop();
-        resolve(null);
-      } catch (err) {
-        reject(err);
-      }
-    });
-  });
+  beforeAll(async () => {});
+  afterAll(async () => {});
   it("Should answer 403 if not logged in", async () => {
     const response = await request(app).delete("/media");
     expect(response.status).toBe(403);
@@ -62,5 +57,24 @@ describe("Route DELETE /media", () => {
       .set("authorization", "Bearer tester");
     expect(response.status).toBe(200);
     expect(response.body).toEqual({});
+  });
+});
+
+describe("Route DELETE /media - media already linked", () => {
+  beforeAll(async () => {
+    await bugMedia.insert({
+      location:
+        "https://s3.eu-west-1.amazonaws.com/media.bucket/media/T1/image_123456789.png",
+    });
+  });
+  afterAll(async () => {});
+  it("Should answer 403 if trying to delete linked media", async () => {
+    const response = await request(app)
+      .delete("/media")
+      .send({
+        url: "https://s3.eu-west-1.amazonaws.com/media.bucket/media/T1/image_123456789.png",
+      })
+      .set("authorization", "Bearer tester");
+    expect(response.status).toBe(403);
   });
 });
