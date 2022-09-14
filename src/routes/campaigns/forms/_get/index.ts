a5e1abba9963a270b1619ed0fac8df93ef506153
Gianpaolo Sinatra <gianpaolo.sinatra@app-quality.com>
2022-09-14 17:24:59 +0200

    feat: should return form with id 3  if searchBy=id,name and search=3

--- src/routes/campaigns/forms/_get/index.ts
+++ src/routes/campaigns/forms/_get/index.ts
@@ -8,12 +8,24 @@ export default class RouteItem extends UserRoute<{
 }> {
   private db: { forms: PreselectionForms };
   private limit: number | undefined;
+  private start: number;
+  private searchBy: ("id" | "name" | "campaign_id")[] | undefined;
+  private search: string | undefined;
 
   constructor(config: RouteClassConfiguration) {
     super(config);
     this.db = { forms: new PreselectionForms(["id", "name", "campaign_id"]) };
     const query = this.getQuery();
     this.limit = parseInt(query.limit as unknown as string) || undefined;
+    this.start = parseInt((query.start as unknown as string) || "0");
+    this.searchBy = query.searchBy
+      ? [...new Set(query.searchBy.split(","))].filter(
+          (value: string): value is "id" | "name" | "campaign_id" => {
+            return ["id", "name", "campaign_id"].includes(value);
+          }
+        )
+      : undefined;
+    this.search = query.search ? query.search : undefined;
   }
 
   protected async filter() {
@@ -27,16 +39,18 @@ export default class RouteItem extends UserRoute<{
   protected async prepare() {
     this.setSuccess(200, {
       results: await this.getForms(),
-      start: 0,
-      size: 0,
+      start: this.start,
+      size: (await this.getForms()).length,
       limit: this.limit,
       total: await this.getTotal(),
     });
   }
 
   private async getForms() {
-    const results = await this.db.forms.query({ limit: this.limit });
-
+    const results = await this.db.forms.query({
+      limit: this.limit,
+      where: this.getWhere(),
+    });
     return results.map((form) => {
       return {
         id: form.id,
@@ -45,8 +59,28 @@ export default class RouteItem extends UserRoute<{
       };
     });
   }
+
   private async getTotal() {
-    const results = await this.db.forms.query({});
+    const results = await this.db.forms.query({ where: this.getWhere() });
     return this.limit ? results.length : undefined;
+  }
+
+  private getWhere() {
+    if (!this.searchBy || !this.search) return undefined;
+    let orQuery: PreselectionForms["where"][number] = [];
+    for (const el of this.searchBy) {
+      switch (el) {
+        case "name":
+          orQuery.push({ name: "%" + this.search + "%", isLike: true });
+          break;
+        case "id":
+        case "campaign_id":
+          orQuery.push({ [el]: parseInt(this.search) });
+          break;
+        default:
+          break;
+      }
+    }
+    return [orQuery];
   }
 }
