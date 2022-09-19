692f7c99fd284638f254905328785ba81dbe4ef3
Davide Bizzi <davide.bizzi@app-quality.com>
2022-09-19 17:24:35 +0200

    test(me/campaigns):  Check for candidature

--- src/routes/users/me/campaigns/_get/index.spec.ts
+++ src/routes/users/me/campaigns/_get/index.spec.ts
@@ -1,56 +1,59 @@
 import request from "supertest";
 import app from "@src/app";
 import campaigns from "@src/__mocks__/mockedDb/campaign";
+import candidature from "@src/__mocks__/mockedDb/cpHasCandidates";
 import campaignTypes from "@src/__mocks__/mockedDb/campaignType";
 import resolvePermalinks from "@src/features/wp/resolvePermalinks";
 
 jest.mock("@src/features/wp/resolvePermalinks");
-describe("GET /users/me/campaigns", () => {
-  const sevenDaysFromNow = new Date().setDate(new Date().getDate() + 7);
-  const endDate = new Date(sevenDaysFromNow).toISOString().split("T")[0];
-  const fourteenDaysFromNow = new Date().setDate(new Date().getDate() + 14);
-  const closeDate = new Date(fourteenDaysFromNow).toISOString().split("T")[0];
-  beforeAll(() => {
-    (resolvePermalinks as jest.Mock).mockImplementation(() => {
-      return {
-        1: { en: "en/test1", it: "it/test1", es: "es/test1" },
-        2: { en: "en/test2", it: "it/test2", es: "es/test2" },
-      };
-    });
-    campaignTypes.insert({
-      id: 1,
-    });
 
-    campaigns.insert({
-      id: 1,
-      title: "Public campaign",
-      start_date: new Date().toISOString().split("T")[0],
-      end_date: endDate,
-      close_date: closeDate,
-      campaign_type_id: 1,
-      page_preview_id: 1,
-      page_manual_id: 2,
-      os: "1",
-      is_public: 1,
-    });
-    campaigns.insert({
-      id: 2,
-      title: "Small Group campaign",
-      start_date: new Date().toISOString().split("T")[0],
-      end_date: endDate,
-      close_date: closeDate,
-      campaign_type_id: 1,
-      page_preview_id: 1,
-      page_manual_id: 2,
-      os: "1",
-      is_public: 0,
-    });
+const sevenDaysFromNow = new Date().setDate(new Date().getDate() + 7);
+const endDate = new Date(sevenDaysFromNow).toISOString().split("T")[0];
+const fourteenDaysFromNow = new Date().setDate(new Date().getDate() + 14);
+const closeDate = new Date(fourteenDaysFromNow).toISOString().split("T")[0];
+
+beforeAll(() => {
+  (resolvePermalinks as jest.Mock).mockImplementation(() => {
+    return {
+      1: { en: "en/test1", it: "it/test1", es: "es/test1" },
+      2: { en: "en/test2", it: "it/test2", es: "es/test2" },
+    };
   });
-  afterAll(() => {
-    campaigns.clear();
-    campaignTypes.clear();
-    jest.resetAllMocks();
+  campaignTypes.insert({
+    id: 1,
   });
+
+  campaigns.insert({
+    id: 1,
+    title: "Public campaign",
+    start_date: new Date().toISOString().split("T")[0],
+    end_date: endDate,
+    close_date: closeDate,
+    campaign_type_id: 1,
+    page_preview_id: 1,
+    page_manual_id: 2,
+    os: "1",
+    is_public: 1,
+  });
+  campaigns.insert({
+    id: 2,
+    title: "Small Group campaign",
+    start_date: new Date().toISOString().split("T")[0],
+    end_date: endDate,
+    close_date: closeDate,
+    campaign_type_id: 1,
+    page_preview_id: 1,
+    page_manual_id: 2,
+    os: "1",
+    is_public: 0,
+  });
+});
+afterAll(() => {
+  campaigns.clear();
+  campaignTypes.clear();
+  jest.resetAllMocks();
+});
+describe("GET /users/me/campaigns", () => {
   it("should answer 403 if not logged in", () => {
     return request(app).get("/users/me/campaigns").expect(403);
   });
@@ -133,5 +136,25 @@ describe("GET /users/me/campaigns", () => {
       it: "it/test1",
       es: "es/test1",
     });
+  });
+});
+
+describe("GET /users/me/campaigns - with application", () => {
+  beforeAll(() => {
+    candidature.insert({
+      user_id: 1,
+      campaign_id: 1,
+      accepted: 0,
+    });
+  });
+  afterAll(() => {
+    candidature.clear();
+  });
+  it("should answer with a single campaign with applied = true", async () => {
+    const response = await request(app)
+      .get("/users/me/campaigns")
+      .set("Authorization", "Bearer tester");
+    expect(response.body).toHaveProperty("results");
+    expect(response.body.results[0]).toHaveProperty("applied", true);
   });
 });
