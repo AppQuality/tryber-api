de347c574c4c59f2b55f88693e4535aee1450f7f
Davide Bizzi <davide.bizzi@app-quality.com>
2022-09-19 14:30:15 +0200

    test(me/campaigns): Add basic test structure

--- src/routes/users/me/campaigns/_get/index.spec.ts
+++ src/routes/users/me/campaigns/_get/index.spec.ts
@@ -15,6 +15,9 @@ jest.mock("@src/features/wp/resolvePermalinks", () => {
 
 describe("GET /users/me/campaigns", () => {
   beforeAll(() => {
+    campaignTypes.insert({
+      id: 1,
+    });
     campaigns.insert({
       id: 1,
       title: "Public campaign",
@@ -27,18 +30,34 @@ describe("GET /users/me/campaigns", () => {
       os: "1",
       is_public: 1,
     });
-    campaignTypes.insert({
-      id: 1,
+    campaigns.insert({
+      id: 2,
+      title: "Small Group campaign",
+      start_date: new Date().toISOString().split("T")[0],
+      end_date: new Date().toISOString().split("T")[0],
+      close_date: new Date().toISOString().split("T")[0],
+      campaign_type_id: 1,
+      page_preview_id: 1,
+      page_manual_id: 2,
+      os: "1",
+      is_public: 0,
     });
+  });
+  afterAll(() => {
+    campaigns.clear();
+    campaignTypes.clear();
   });
   it("should answer 403 if not logged in", () => {
     return request(app).get("/users/me/campaigns").expect(403);
   });
 
-  it("should answer ", async () => {
+  it("should answer with a single campaign", async () => {
     const response = await request(app)
       .get("/users/me/campaigns")
       .set("Authorization", "Bearer tester");
-    console.log(response.body);
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty("results");
+    expect(Array.isArray(response.body.results)).toBe(true);
+    expect(response.body.results.length).toBe(1);
   });
 });
