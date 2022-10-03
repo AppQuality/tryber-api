cb33c318979eda912dad11b1265f9d88f9a858b9
Gianpaolo Sinatra <gianpaolo.sinatra@app-quality.com>
2022-10-03 10:56:07 +0200

    refactor get compatible devices

--- src/routes/users/me/campaigns/campaignId/available_devices/_get/index.ts
+++ src/routes/users/me/campaigns/campaignId/available_devices/_get/index.ts
@@ -1,5 +1,5 @@
 import UserRoute from "@src/features/routes/UserRoute";
-import Campaigns from "@src/features/db/class/Campaigns";
+import Campaigns, { CampaignObject } from "@src/features/db/class/Campaigns";
 import PageAccess from "@src/features/db/class/PageAccess";
 
 /** OPENAPI-CLASS: get-users-me-campaigns-campaignId-compatible-devices */
@@ -12,13 +12,12 @@ class RouteItem extends UserRoute<{
   response: StoplightOperations["get-users-me-campaigns-campaignId-compatible-devices"]["responses"]["200"]["content"]["application/json"];
 }> {
   private campaign_id: number;
-  private db: { campaigns: Campaigns; lc_access: PageAccess };
-  private campaign: Awaited<ReturnType<Campaigns["query"]>>[number] | false =
-    false;
+  private db: { campaigns: Campaigns; page_access: PageAccess };
+  private campaign: CampaignObject | undefined;
 
   constructor(configuration: RouteClassConfiguration) {
     super(configuration);
-    this.db = { campaigns: new Campaigns(), lc_access: new PageAccess() };
+    this.db = { campaigns: new Campaigns(), page_access: new PageAccess() };
     this.campaign_id = parseInt(this.getParameters().campaign);
   }
   protected async filter() {
@@ -59,25 +58,7 @@ class RouteItem extends UserRoute<{
     return (await this.getCampaign()).start_date >= today;
   }
   private async userCanAccessToForm() {
-    return (
-      (await this.campaignIsPublic()) ||
-      ((await this.isSmallGroupAccess()) && (await this.isUserInSmallGroup()))
-    );
-  }
-  private async isUserInSmallGroup(): Promise<boolean> {
-    const previewAccess = await this.db.lc_access.query({
-      where: [
-        { tester_id: this.getTesterId() },
-        { view_id: parseInt((await this.getCampaign()).page_preview_id) },
-      ],
-    });
-    return !!previewAccess.length === true;
-  }
-  private async campaignIsPublic(): Promise<boolean> {
-    return (await this.getCampaign()).is_public === 1;
-  }
-  private async isSmallGroupAccess(): Promise<boolean> {
-    return (await this.getCampaign()).is_public === 3;
+    return (await this.getCampaign()).testerHasAccess(this.getTesterId());
   }
   private async getCampaign() {
     if (!this.campaign) {
