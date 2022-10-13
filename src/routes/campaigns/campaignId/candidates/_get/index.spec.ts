d354c665024700022969966a9119b72f9a7ce2f5
Davide Bizzi <davide.bizzi@app-quality.com>
2022-10-13 16:51:11 +0200

    wip: Add test for candidature tester id

--- src/routes/campaigns/campaignId/candidates/_get/index.spec.ts
+++ src/routes/campaigns/campaignId/candidates/_get/index.spec.ts
@@ -1,8 +1,37 @@
 import request from "supertest";
 import app from "@src/app";
+import Campaigns from "@src/__mocks__/mockedDb/campaign";
+import Candidate from "@src/__mocks__/mockedDb/cpHasCandidates";
+import Profile from "@src/__mocks__/mockedDb/profile";
+import WpUsers from "@src/__mocks__/mockedDb/wp_users";
+
 describe("GET /campaigns/:campaignId/candidates ", () => {
-  beforeAll(async () => {});
-  afterAll(async () => {});
+  beforeAll(async () => {
+    await Campaigns.insert({ id: 1 });
+    await Profile.insert({ id: 1 });
+    await WpUsers.insert({ ID: 1 });
+    await Profile.insert({ id: 4, wp_user_id: 5 });
+    await WpUsers.insert({ ID: 5 });
+    await Profile.insert({ id: 2, wp_user_id: 6 });
+    await WpUsers.insert({ ID: 6 });
+    await Profile.insert({ id: 3, wp_user_id: 7 });
+    await WpUsers.insert({ ID: 7 });
+    await Candidate.insert({
+      user_id: 5,
+      campaign_id: 1,
+    });
+    await Candidate.insert({
+      user_id: 6,
+      campaign_id: 1,
+    });
+    await Candidate.insert({
+      user_id: 7,
+      campaign_id: 1,
+    });
+  });
+  afterAll(async () => {
+    await Campaigns.clear();
+  });
   it("should answer 403 if user is not logged in ", async () => {
     const response = await request(app).get("/campaigns/1/candidates");
 
@@ -19,5 +48,32 @@ describe("GET /campaigns/:campaignId/candidates ", () => {
       .get("/campaigns/1/candidates/")
       .set("authorization", `Bearer tester olp {"appq_tester_selection":true}`);
     expect(response.status).toBe(200);
+  });
+  it("should answer 404 if campaign does not exists", async () => {
+    const response = await request(app)
+      .get("/campaigns/100/candidates/")
+      .set("authorization", `Bearer tester olp {"appq_tester_selection":true}`);
+    expect(response.status).toBe(404);
+  });
+  it("should answer a list of tester ids ", async () => {
+    const response = await request(app)
+      .get("/campaigns/1/candidates/")
+      .set("authorization", `Bearer tester olp {"appq_tester_selection":true}`);
+    expect(response.body).toHaveProperty("results");
+    expect(response.body.results.length).toBe(3);
+    expect(response.body.results).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          tester_id: 4,
+        }),
+        expect.objectContaining({
+          tester_id: 2,
+        }),
+
+        expect.objectContaining({
+          tester_id: 3,
+        }),
+      ])
+    );
   });
 });
--- src/routes/campaigns/campaignId/candidates/_get/index.ts
+++ src/routes/campaigns/campaignId/candidates/_get/index.ts
@@ -1,26 +1,42 @@
 /** OPENAPI-CLASS: get-campaigns-campaign-candidates */
 import UserRoute from "@src/features/routes/UserRoute";
 import OpenapiError from "@src/features/OpenapiError";
+import Campaigns from "@src/features/db/class/Campaigns";
 
 export default class RouteItem extends UserRoute<{
   response: StoplightOperations["get-campaigns-campaign-candidates"]["responses"][200]["content"]["application/json"];
   parameters: StoplightOperations["get-campaigns-campaign-candidates"]["parameters"]["path"];
 }> {
   private campaign_id: number;
+  private db: {
+    campaigns: Campaigns;
+  };
 
   constructor(config: RouteClassConfiguration) {
     super(config);
     const parameters = this.getParameters();
     this.campaign_id = parseInt(parameters.campaign);
+    this.db = {
+      campaigns: new Campaigns(),
+    };
   }
 
   protected async filter() {
     if (this.hasAccessTesterSelection(this.campaign_id) === false) {
-      this.setError(403, new Error("You are not authorized.") as OpenapiError);
+      this.setError(403, new OpenapiError("You are not authorized."));
+      return false;
+    }
+    if ((await this.campaignExists()) === false) {
+      this.setError(404, new OpenapiError("Campaign does not exists."));
       return false;
     }
     return true;
   }
+
+  private async campaignExists() {
+    return await this.db.campaigns.exists(this.campaign_id);
+  }
+
   protected async prepare() {
     this.setSuccess(200, {
       results: [
