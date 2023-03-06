8b32f049e53660c59b1be96d00328cac16deb7a1
Gianpaolo Sinatra <gianpaolo.sinatra@app-quality.com>
2023-03-06 12:27:34 +0100

    feat(update mock table definition): usecases

--- src/__mocks__/mockedDb/usecases.ts
+++ src/__mocks__/mockedDb/usecases.ts
@@ -1,32 +1,59 @@
 import Table from "./table";
 
-type UseCasesParams = {
+type UseCaseParams = {
   id?: number;
   title?: string;
+  content?: string;
   campaign_id?: number;
+  is_required?: number;
+  jf_code?: string;
+  jf_text?: string;
   group_id?: number;
   position?: number;
+  allow_media?: number;
+  optimize_media?: number;
+  simple_title?: string;
+  info?: string;
+  prefix?: string;
 };
-class UseCases extends Table<UseCasesParams> {
+
+const defaultItem: UseCaseParams = {
+  title: "???",
+  content: "???",
+  campaign_id: 0,
+  is_required: 1,
+  jf_code: "???",
+  jf_text: "???",
+  group_id: 1,
+  position: 0,
+  allow_media: 0,
+  optimize_media: 0,
+  simple_title: "",
+  info: "",
+  prefix: "",
+};
+class UseCases extends Table<UseCaseParams> {
   protected name = "wp_appq_campaign_task";
   protected columns = [
-    "id INTEGER PRIMARY KEY",
-    "title VARCHAR(255)",
-    "campaign_id INTEGER(11)",
-    "group_id INTEGER(11)",
-    "position INTEGER(11)",
+    "id INTEGER PRIMARY KEY AUTOINCREMENT",
+    "title VARCHAR(512)",
+    "content TEXT",
+    "campaign_id INTEGER",
+    "is_required INTEGER",
+    "jf_code VARCHAR(15)",
+    "jf_text VARCHAR(60)",
+    "group_id INTEGER DEFAULT 1 NOT NULL",
+    "position INTEGER DEFAULT 0 NOT NULL",
+    "allow_media INTEGER DEFAULT 0 NOT NULL",
+    "optimize_media INTEGER DEFAULT 0 NOT NULL",
+    "simple_title VARCHAR(128) NOT NULL",
+    "info VARCHAR(256) NOT NULL",
+    "prefix VARCHAR(128) NOT NULL",
   ];
   constructor() {
-    super({
-      id: 1,
-      title: "My Usecase",
-      campaign_id: 1,
-      group_id: -1,
-      position: 0,
-    });
+    super(defaultItem);
   }
 }
-
-const theTable = new UseCases();
-
-export default theTable;
+const useCases = new UseCases();
+export default useCases;
+export type { UseCaseParams };
