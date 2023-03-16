c294460c1ebdd0ce92cd368066946bdad61f89e8
Davide Bizzi <davide.bizzi@app-quality.com>
2023-03-16 11:38:05 +0100

    chore: Add comments for features

--- src/routes/campaigns/campaignId/prospect/index.spec.ts
+++ src/routes/campaigns/campaignId/prospect/index.spec.ts
@@ -62,6 +62,21 @@ beforeAll(async () => {
     id: 1,
     name: "Crash",
   });
+  /**
+      meta_id, campaign_id, meta_key, meta_value
+      87189,5453,point_multiplier_medium,0.1
+      87188,5453,point_multiplier_low,0.05
+      87187,5453,critical_bug_payout,6
+      87186,5453,high_bug_payout,2.5
+      87185,5453,medium_bug_payout,1
+      87184,5453,low_bug_payout,0.5
+      87183,5453,payout_limit,30
+      87182,5453,campaign_complete_bonus_eur,15
+
+      wp_campaign
+      id, campaign_pts
+      5453, 100
+   */
   await tryber.tables.WpAppqCpMeta.do().insert({});
 });
 
@@ -94,6 +109,8 @@ describe("GET /campaigns/campaignId/prospect", () => {
       );
     expect(response.status).toBe(200);
   });
+
+  // should not return not candidate testers
 });
 
 describe("GET /campaigns/campaignId/prospect - tester payouts were edit", () => {
@@ -117,9 +134,38 @@ describe("GET /campaigns/campaignId/prospect - tester payouts were edit", () => 
     await tryber.tables.WpAppqProspectPayout.do().delete();
   });
 
-  beforeEach(async () => {
-    await tryber.tables.WpAppqProspectPayout.do().update({ is_edit: 1 });
+  it("Should return 200 ", async () => {
+    const response = await request(app)
+      .get("/campaigns/1/prospect")
+      .set(
+        "Authorization",
+        'Bearer tester olp {"appq_tester_selection":[1],"appq_prospect":[1]}'
+      );
+    expect(response.status).toBe(200);
   });
+});
+
+describe("GET /campaigns/campaignId/prospect - there is an edit in old prospect", () => {
+  beforeAll(async () => {
+    await tryber.tables.WpAppqProspectPayout.do().insert({
+      id: 1,
+      campaign_id: 1,
+      tester_id: 1,
+      complete_pts: 1,
+      extra_pts: 1,
+      complete_eur: 1,
+      bonus_bug_eur: 1,
+      extra_eur: 1,
+      refund: 1,
+      notes: "This is the notes",
+      is_edit: 1,
+    });
+  });
+
+  afterAll(async () => {
+    await tryber.tables.WpAppqProspectPayout.do().delete();
+  });
+
   it("Should return 412 if some tester payout was edit", async () => {
     const response = await request(app)
       .get("/campaigns/1/prospect")
