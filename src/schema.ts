763472ce00cddf1718b5d99d4a6711f09844f58f
Gianpaolo Sinatra <gianpaolo.sinatra@app-quality.com>
2023-03-20 17:04:37 +0100

    update schema

--- src/schema.ts
+++ src/schema.ts
@@ -1169,7 +1169,11 @@ export interface operations {
     };
     responses: {
       /** OK */
-      200: unknown;
+      200: {
+        content: {
+          "application/json": { [key: string]: unknown };
+        };
+      };
       /** Not Modified */
       304: never;
     };
