2cfe40fc346e05ce83b658e55e6ecdd8fbe4a954
Gianpaolo Sinatra <gianpaolo.sinatra@app-quality.com>
2023-04-01 17:09:48 +0200

    update schema

--- src/schema.ts
+++ src/schema.ts
@@ -1356,10 +1356,6 @@ export interface operations {
       path: {
         campaign: string;
       };
-      query: {
-        /** Key-value Array for item filtering */
-        filterBy?: components["parameters"]["filterBy"];
-      };
     };
     responses: {
       /** OK */
