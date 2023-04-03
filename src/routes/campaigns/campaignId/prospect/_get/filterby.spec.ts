6c2c78a02868281d686367bf10052b50cc0cf0f3
Gianpaolo Sinatra <gianpaolo.sinatra@app-quality.com>
2023-04-03 09:21:46 +0200

    rename tests

--- src/routes/campaigns/campaignId/prospect/_get/filterby.spec.ts
+++ src/routes/campaigns/campaignId/prospect/_get/filterby.spec.ts
@@ -156,7 +156,7 @@ describe("GET /campaigns/campaignId/prospect - tester payouts were edit", () => 
 });
 
 describe("GET /campaigns/campaignId/prospect - there are no record", () => {
-  it("Should return basic data for bugs payout filtered by including id list", async () => {
+  it("Should return prospect filtered by including id list", async () => {
     const response = await request(app)
       .get("/campaigns/1/prospect?filterByInclude[ids]=2,0,3")
       .set("Authorization", 'Bearer tester olp {"appq_tester_selection":[1]}');
@@ -173,7 +173,7 @@ describe("GET /campaigns/campaignId/prospect - there are no record", () => {
     );
   });
 
-  it("Should return basic data for bugs payout filtered by excluding id list", async () => {
+  it("Should return prospect filtered by excluding id list", async () => {
     const response = await request(app)
       .get("/campaigns/1/prospect?filterByExclude[ids]=0,3")
       .set("Authorization", 'Bearer tester olp {"appq_tester_selection":[1]}');
