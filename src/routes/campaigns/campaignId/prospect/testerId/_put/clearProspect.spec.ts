c712941999b2cb4a2f3ef30f723672e234415433
Davide Bizzi <davide.bizzi@unguess.io>
2023-04-05 16:42:21 +0200

    feat: Save prospect on edit

--- src/routes/campaigns/campaignId/prospect/testerId/_put/clearProspect.spec.ts
+++ src/routes/campaigns/campaignId/prospect/testerId/_put/clearProspect.spec.ts
@@ -97,4 +97,39 @@ describe("PUT /campaigns/:campaignId/prospects/:testerId - without a propect dat
       })
     );
   });
+
+  it("should insert a prospect", async () => {
+    const response = await request(app)
+      .put("/campaigns/1/prospect/1")
+      .send({
+        payout: {
+          completion: 10.5,
+          bugs: 7.5,
+          refund: 5,
+          extra: 5,
+        },
+        experience: {
+          completion: 100,
+          extra: 3,
+        },
+        note: "Note",
+        completed: true,
+      })
+      .set("Authorization", 'Bearer tester olp {"appq_tester_selection":true}');
+
+    const prospect = await tryber.tables.WpAppqProspect.do()
+      .select()
+      .where("campaign_id", 1);
+    expect(prospect.length).toBe(1);
+
+    const prospectItem = await tryber.tables.WpAppqProspectPayout.do()
+      .select()
+      .where("campaign_id", 1);
+    expect(prospectItem.length).toBe(1);
+    expect(prospectItem[0]).toEqual(
+      expect.objectContaining({
+        prospect_id: prospect[0].id,
+      })
+    );
+  });
 });
