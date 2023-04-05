13a771e36665369845e88494c3a3a9eec3a0ab1b
Gianpaolo Sinatra <gianpaolo.sinatra@app-quality.com>
2023-04-05 08:41:00 +0200

    update schema

--- src/schema.ts
+++ src/schema.ts
@@ -542,11 +542,8 @@ export interface components {
       projectManager?: components["schemas"]["User"];
       customerCanViewReviewing?: boolean;
       additionalFields?: components["schemas"]["CampaignField"][];
-      /** @default 0 */
       tokens?: number;
-      /** @default 0 */
       csm_effort?: number;
-      /** @default 0 */
       ux_effort?: number;
       preview_link?: components["schemas"]["TranslatablePage"];
       manual_link?: components["schemas"]["TranslatablePage"];
@@ -1501,6 +1498,8 @@ export interface operations {
       200: unknown;
       /** Not Modified */
       304: never;
+      /** Not Implemented */
+      501: unknown;
     };
     requestBody: {
       content: {
@@ -1536,8 +1535,8 @@ export interface operations {
     responses: {
       /** OK */
       200: unknown;
-      /** Not Modified */
-      304: never;
+      /** Not Implemented */
+      501: unknown;
     };
     requestBody: {
       content: {
