9c808de0cd0a141c486acad7f4056a206f4347ad
Gianpaolo Sinatra <gianpaolo.sinatra@app-quality.com>
2023-04-05 15:15:27 +0200

    wip

--- src/routes/campaigns/campaignId/prospect/_patch/index.spec.ts
+++ src/routes/campaigns/campaignId/prospect/_patch/index.spec.ts
@@ -17,14 +17,21 @@ beforeAll(async () => {
     customer_title: "",
     campaign_pts: 200,
   });
+});
+beforeEach(async () => {
   await tryber.tables.WpAppqProspect.do().insert({
     id: 1,
     campaign_id: 1,
     status: "draft",
+    last_modified: "",
   });
 });
 
-describe("PATCH /campaigns/campaignId/prospect", () => {
+afterEach(async () => {
+  await tryber.tables.WpAppqProspect.do().delete();
+});
+
+describe("PATCH /campaigns/campaignId/prospect with one prospect", () => {
   it("Should return 304 if send empty object", async () => {
     const response = await request(app)
       .patch("/campaigns/1/prospect")
@@ -73,34 +80,91 @@ describe("PATCH /campaigns/campaignId/prospect", () => {
     expect(response.status).toBe(200);
   });
 
-  // GIVEN:
-  /*
-
-
-  */
-  // WHEN:
-  /*
-
-
-  */
-  // THEN:
-  /*
-
-
-
-  */
-  it("Should return 200 if logged in as tester with both olps tester_selection", async () => {
+  it("Should return 200 if logged in as tester with olp tester_selection", async () => {
+    console.log(await tryber.tables.WpAppqProspect.do().select());
     const response = await request(app)
       .patch("/campaigns/1/prospect")
       .send({ status: "confirmed" })
-      .set("Authorization", 'Bearer tester olp {"appq_tester_selection":[1] }');
-    //expect(response.status).toBe(200);
-    console.log(response.status);
+      .set("Authorization", 'Bearer tester olp {"appq_tester_selection":[1]}');
+    expect(response.status).toBe(200);
     const prospect = await tryber.tables.WpAppqProspect.do()
       .select()
       .where({ campaign_id: 1 })
       .first();
-    console.log(prospect);
+    expect(prospect?.status).toBe("confirmed");
+  });
+});
+
+describe("PATCH /campaigns/campaignId/prospect when no prospect", () => {
+  // campaign_id should me unique \
+
+  //if send the same status retrun 304
+  // if there no prospect same tests...
+  // on_duplicate_key_update unguess analitics
+
+  beforeEach(async () => {
+    await tryber.tables.WpAppqProspect.do().delete();
+  });
+
+  it("Should return 304 if send empty object", async () => {
+    const response = await request(app)
+      .patch("/campaigns/1/prospect")
+      .send({})
+      .set("Authorization", "Bearer admin");
+    expect(response.status).toBe(304);
+  });
+
+  it("Should return 501 send status done", async () => {
+    const response = await request(app)
+      .patch("/campaigns/1/prospect")
+      .send({ status: "done" })
+      .set("Authorization", "Bearer admin");
+    expect(response.status).toBe(501);
+  });
+
+  it("Should return 403 if logged out", async () => {
+    const response = await request(app)
+      .patch("/campaigns/1/prospect")
+      .send({ status: "confirmed" });
+    expect(response.status).toBe(403);
+  });
+
+  it("Should return 403 if logged in as not admin user", async () => {
+    const response = await request(app)
+      .patch("/campaigns/1/prospect")
+      .send({ status: "confirmed" })
+      .set("Authorization", "Bearer tester");
+    expect(response.status).toBe(403);
+  });
+
+  it("Should return 400 if campaign does not exists", async () => {
+    const response = await request(app)
+      .patch("/campaigns/100/prospect")
+      .send({ status: "confirmed" })
+      .set("Authorization", "Bearer tester");
+
+    expect(response.status).toBe(400);
+  });
+
+  it("Should return 200 if logged in as admin", async () => {
+    const response = await request(app)
+      .patch("/campaigns/1/prospect")
+      .send({ status: "confirmed" })
+      .set("Authorization", "Bearer admin");
+    expect(response.status).toBe(200);
+  });
+
+  it("Should return 200 if logged in as tester with olp tester_selection", async () => {
+    console.log(await tryber.tables.WpAppqProspect.do().select());
+    const response = await request(app)
+      .patch("/campaigns/1/prospect")
+      .send({ status: "confirmed" })
+      .set("Authorization", 'Bearer tester olp {"appq_tester_selection":[1]}');
+    expect(response.status).toBe(200);
+    const prospect = await tryber.tables.WpAppqProspect.do()
+      .select()
+      .where({ campaign_id: 1 })
+      .first();
     expect(prospect?.status).toBe("confirmed");
   });
 });
