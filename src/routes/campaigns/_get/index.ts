9d749ca72457af40e9c099ebd4001244cc948beb
Gianpaolo Sinatra <gianpaolo.sinatra@app-quality.com>
2023-04-07 18:16:19 +0200

    refact(get-campaigns): remove db and use knex

--- src/routes/campaigns/_get/index.ts
+++ src/routes/campaigns/_get/index.ts
@@ -1,7 +1,7 @@
 /** OPENAPI-CLASS : get-campaigns */
 
 import UserRoute from "@src/features/routes/UserRoute";
-import * as db from "@src/features/db";
+import { tryber } from "@src/features/database";
 class RouteItem extends UserRoute<{
   response: StoplightOperations["get-campaigns"]["responses"]["200"]["content"]["application/json"];
 }> {
@@ -25,29 +25,25 @@ class RouteItem extends UserRoute<{
     }
     return true;
   }
-  protected async prepare() {
-    try {
-      this.setSuccess(200, await this.getCampaigns());
-    } catch (e) {
-      const error = e as OpenapiError;
-      this.setError(error.status_code || 500, error);
-    }
+
+  protected async prepare(): Promise<void> {
+    return this.setSuccess(200, await this.getCampaigns());
   }
 
   private async getCampaigns() {
-    let result: { id: number; name: string }[] = [];
+    let result = tryber.tables.WpAppqEvdCampaign.do().select(
+      "id",
+      tryber.ref("title").withSchema("wp_appq_evd_campaign").as("name")
+    );
+
     if (this.accessibleCampaigns === true) {
-      result = await db.query(
-        "SELECT id, title as name FROM wp_appq_evd_campaign"
-      );
-    } else {
-      result = await db.query(
-        `SELECT id, title as name 
-          FROM wp_appq_evd_campaign 
-          WHERE id IN (${this.accessibleCampaigns.join(",")})`
-      );
+      return await result;
     }
-    return result;
+    if (this.accessibleCampaigns.length > 0) {
+      return await result.whereIn("id", this.accessibleCampaigns);
+    }
+
+    return [];
   }
 }
 
