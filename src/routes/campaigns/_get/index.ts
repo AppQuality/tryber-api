85a7b078b03b3cfdfe4ad995427aae19756fc963
Davide Bizzi <davide.bizzi@unguess.io>
2023-05-02 15:25:34 +0200

    feat: Add search by customer title

--- src/routes/campaigns/_get/index.ts
+++ src/routes/campaigns/_get/index.ts
@@ -255,11 +255,9 @@ class RouteItem extends UserRoute<{
       if (this.search) {
         const search = this.search.toLowerCase();
         query = query.where(function () {
-          this.whereLike("wp_appq_evd_campaign.id", `%${search}%`).orWhere(
-            "wp_appq_evd_campaign.title",
-            "like",
-            `%${search}%`
-          );
+          this.whereLike("wp_appq_evd_campaign.id", `%${search}%`)
+            .orWhereLike("wp_appq_evd_campaign.title", `%${search}%`)
+            .orWhereLike("wp_appq_evd_campaign.customer_title", `%${search}%`);
         });
       }
     });
