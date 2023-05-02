a166ea82c2052753d82356a3ba2a81fcae233da7
Davide Bizzi <davide.bizzi@unguess.io>
2023-05-02 16:01:17 +0200

    test: Add tests for order

--- src/routes/campaigns/_get/order.spec.ts
+++ src/routes/campaigns/_get/order.spec.ts
@@ -1,15 +1,20 @@
 import app from "@src/app";
 import { tryber } from "@src/features/database";
 import request from "supertest";
+
+const today = new Date().toISOString().split("T")[0];
+const yesterday = new Date(new Date().getTime() - 24 * 60 * 60 * 1000)
+  .toISOString()
+  .split("T")[0];
+const tomorrow = new Date(new Date().getTime() + 24 * 60 * 60 * 1000)
+  .toISOString()
+  .split("T")[0];
+
 const campaign = {
   id: 1,
   platform_id: 1,
-  start_date: new Date(new Date().getTime() - 24 * 60 * 60 * 1000)
-    .toISOString()
-    .split("T")[0],
-  end_date: new Date(new Date().getTime() + 24 * 60 * 60 * 1000)
-    .toISOString()
-    .split("T")[0],
+  start_date: today,
+  end_date: today,
   title: "This is the title",
   page_preview_id: 1,
   page_manual_id: 1,
@@ -33,9 +38,27 @@ describe("GET /campaigns", () => {
       },
     ]);
     await tryber.tables.WpAppqEvdCampaign.do().insert([
-      { ...campaign, id: 1, title: "First campaign" },
-      { ...campaign, id: 2, title: "Second campaign" },
-      { ...campaign, id: 3, title: "Third campaign" },
+      {
+        ...campaign,
+        id: 1,
+        start_date: today,
+        end_date: yesterday,
+        title: "First campaign",
+      },
+      {
+        ...campaign,
+        id: 2,
+        start_date: yesterday,
+        end_date: tomorrow,
+        title: "Second campaign",
+      },
+      {
+        ...campaign,
+        id: 3,
+        start_date: tomorrow,
+        end_date: today,
+        title: "Third campaign",
+      },
     ]);
   });
   afterAll(async () => {
@@ -52,5 +75,58 @@ describe("GET /campaigns", () => {
     expect(response.body.items[0].id).toBe(3);
     expect(response.body.items[1].id).toBe(2);
     expect(response.body.items[2].id).toBe(1);
+  });
+
+  it("Should order by id by default", async () => {
+    const response = await request(app)
+      .get("/campaigns?order=ASC")
+      .set("Authorization", 'Bearer tester olp {"appq_campaign":true}');
+    expect(response.status).toBe(200);
+    expect(response.body.items).toHaveLength(3);
+    expect(response.body.items[0].id).toBe(1);
+    expect(response.body.items[1].id).toBe(2);
+    expect(response.body.items[2].id).toBe(3);
+  });
+
+  it("Should allow ordering by start date, default DESC", async () => {
+    const response = await request(app)
+      .get("/campaigns?orderBy=startDate")
+      .set("Authorization", 'Bearer tester olp {"appq_campaign":true}');
+    expect(response.status).toBe(200);
+    expect(response.body.items).toHaveLength(3);
+    expect(response.body.items[0].id).toBe(3);
+    expect(response.body.items[1].id).toBe(1);
+    expect(response.body.items[2].id).toBe(2);
+  });
+  it("Should allow ordering by start date, ASC", async () => {
+    const response = await request(app)
+      .get("/campaigns?orderBy=startDate&order=ASC")
+      .set("Authorization", 'Bearer tester olp {"appq_campaign":true}');
+    expect(response.status).toBe(200);
+    expect(response.body.items).toHaveLength(3);
+    expect(response.body.items[0].id).toBe(2);
+    expect(response.body.items[1].id).toBe(1);
+    expect(response.body.items[2].id).toBe(3);
+  });
+
+  it("Should allow ordering by end date, default DESC", async () => {
+    const response = await request(app)
+      .get("/campaigns?orderBy=endDate")
+      .set("Authorization", 'Bearer tester olp {"appq_campaign":true}');
+    expect(response.status).toBe(200);
+    expect(response.body.items).toHaveLength(3);
+    expect(response.body.items[0].id).toBe(2);
+    expect(response.body.items[1].id).toBe(3);
+    expect(response.body.items[2].id).toBe(1);
+  });
+  it("Should allow ordering by end date, ASC", async () => {
+    const response = await request(app)
+      .get("/campaigns?orderBy=endDate&order=ASC")
+      .set("Authorization", 'Bearer tester olp {"appq_campaign":true}');
+    expect(response.status).toBe(200);
+    expect(response.body.items).toHaveLength(3);
+    expect(response.body.items[0].id).toBe(1);
+    expect(response.body.items[1].id).toBe(3);
+    expect(response.body.items[2].id).toBe(2);
   });
 });
