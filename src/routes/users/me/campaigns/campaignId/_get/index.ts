1afd26daf1d1a5b40d3e92ff078f04cf9fe0e4ef
Gianpaolo Sinatra <gianpaolo.sinatra@app-quality.com>
2023-05-19 10:25:23 +0200

    rework(openapi-route-to-class): set as class route users-me-campaigns-id

--- src/routes/users/me/campaigns/campaignId/_get/index.ts
+++ src/routes/users/me/campaigns/campaignId/_get/index.ts
@@ -1,61 +1,77 @@
-/** OPENAPI-ROUTE: get-users-me-campaigns-campaignId */
+/** OPENAPI-CLASS: get-users-me-campaigns-campaignId */
 
-import { Context } from "openapi-backend";
 import Campaign from "@src/features/class/Campaign";
-import { Result } from "./types.d";
+import UserRoute from "@src/features/routes/UserRoute";
 
-export default async (
-  c: Context,
-  req: OpenapiRequest,
-  res: OpenapiResponse
-): Promise<Result | ReturnErrorType> => {
-  const params = c.request.params as PathParameters;
-  const campaignId = parseInt(params.campaignId);
-  const campaign = new Campaign(campaignId, false);
-  try {
-    if (!(await campaign.isUserCandidate(req.user.ID, isAdmin())))
-      throw new Error("You are not selected for this campaign");
-  } catch {
-    res.status_code = 404;
-    return {
-      id: campaignId,
-      element: "campaigns",
-      message: "You don't have access to a campaign with this id",
-    };
-  }
-  try {
-    campaign.init();
-    await campaign.ready;
-    res.status_code = 200;
-    return {
-      id: campaign.id,
-      title: campaign.title,
-      minimumMedia: campaign.min_allowed_media,
-      hasBugForm: campaign.hasBugForm,
-      bugSeverity: await campaign.getAvailableSeverities(),
-      bugReplicability: await campaign.getAvailableReplicabilities(),
-      useCases: await campaign.getUserUseCases(req.user.ID),
-      bugTypes: await campaign.getAvailableTypes(),
-      validFileExtensions: await campaign.getAvailableFileExtensions(),
-      additionalFields: await campaign.getAdditionalFields(),
-      language: await campaign.getBugLanguageMessage(),
-      titleRule: await campaign.getTitleRule(),
-    };
-  } catch (err) {
-    res.status_code = 500;
-    return {
-      id: campaignId,
-      element: "campaigns",
-      message: (err as OpenapiError).message,
-    };
+export default class UserSingleCampaignRoute extends UserRoute<{
+  response: StoplightOperations["get-users-me-campaigns-campaignId"]["responses"]["200"]["content"]["application/json"];
+  parameters: StoplightOperations["get-users-me-campaigns-campaignId"]["parameters"]["path"];
+}> {
+  private campaign: Campaign;
+
+  constructor(protected configuration: RouteClassConfiguration) {
+    super({ ...configuration, element: "campaigns" });
+    const params = this.getParameters();
+    const campaignId = parseInt(params.campaignId);
+    this.campaign = new Campaign(campaignId, false);
   }
 
-  function isAdmin(): any {
-    if (!req.user.permission.admin) return false;
-    if (!req.user.permission.admin.appq_campaign) return false;
-    if (req.user.permission.admin.appq_campaign === true) return true;
-    if (req.user.permission.admin.appq_campaign.includes(campaignId))
+  protected async init(): Promise<void> {
+    this.campaign.init();
+    await this.campaign.ready;
+  }
+
+  protected async filter(): Promise<boolean> {
+    if (
+      !(await this.campaign.isUserCandidate(
+        this.getWordpressId().toString(),
+        this.isAdmin()
+      ))
+    ) {
+      this.setError(
+        404,
+        "You are not selected for this campaign" as unknown as OpenapiError
+      );
+      return false;
+    }
+    return true;
+  }
+
+  protected async prepare() {
+    try {
+      this.setSuccess(200, {
+        id: this.campaign.id,
+        title: this.campaign.title,
+        minimumMedia: this.campaign.min_allowed_media,
+        hasBugForm: this.campaign.hasBugForm,
+        bugSeverity: await this.campaign.getAvailableSeverities(),
+        bugReplicability: await this.campaign.getAvailableReplicabilities(),
+        useCases: await this.campaign.getUserUseCases(
+          this.getWordpressId().toString()
+        ),
+        bugTypes: await this.campaign.getAvailableTypes(),
+        validFileExtensions: await this.campaign.getAvailableFileExtensions(),
+        additionalFields: await this.campaign.getAdditionalFields(),
+        language: await this.campaign.getBugLanguageMessage(),
+        titleRule: await this.campaign.getTitleRule(),
+      });
+    } catch (error) {
+      this.setError(500, error as OpenapiError);
+    }
+  }
+
+  private isAdmin() {
+    if (!this.configuration.request.user.permission.admin) return false;
+    if (!this.configuration.request.user.permission.admin.appq_campaign)
+      return false;
+    if (this.configuration.request.user.permission.admin.appq_campaign === true)
+      return true;
+    if (
+      this.configuration.request.user.permission.admin.appq_campaign.includes(
+        this.campaign.id
+      )
+    )
       return true;
     return false;
   }
-};
+}
