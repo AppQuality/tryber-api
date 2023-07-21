5d6269f7d2875a34a4f0ee0100b55bf4be941002
Gianpaolo Sinatra <gianpaolo.sinatra@app-quality.com>
2023-07-21 09:14:34 +0200

    feat(get-observations): permission tests

---
+++ src/routes/campaigns/campaignId/observations/_get/index.spec.ts
@@ -0,0 +1,58 @@
+import request from "supertest";
+import app from "@src/app";
+import { tryber } from "@src/features/database";
+
+beforeAll(async () => {
+  await tryber.tables.WpAppqEvdCampaign.do().insert({
+    id: 1,
+    platform_id: 1,
+    start_date: "2020-01-01",
+    end_date: "2020-01-01",
+    title: "This is the title",
+    page_preview_id: 1,
+    page_manual_id: 1,
+    customer_id: 1,
+    pm_id: 1,
+    project_id: 1,
+    customer_title: "",
+    campaign_pts: 200,
+  });
+});
+describe("GET /campaigns/:campaignId/observations", () => {
+  it("Should return 400 if campaign does not exist", async () => {
+    const response = await request(app)
+      .get("/campaigns/999/observations")
+      .set("Authorization", "Bearer admin");
+    expect(response.status).toBe(400);
+  });
+  it("Should return 403 if the user does not have permission", async () => {
+    const response = await request(app)
+      .get("/campaigns/1/observations")
+      .set("Authorization", "Bearer tester");
+    expect(response.status).toBe(403);
+  });
+
+  it("Should return 200 if the user have olp permission on CP", async () => {
+    const response = await request(app)
+      .get("/campaigns/1/observations")
+      .set("Authorization", 'Bearer tester olp {"appq_campaign":[1]}');
+    console.log(response.body);
+    expect(response.status).toBe(200);
+  });
+
+  it("Should return 200 if the user have olp permission on all CP", async () => {
+    const response = await request(app)
+      .get("/campaigns/1/observations")
+      .set("Authorization", 'Bearer tester olp {"appq_campaign":true}');
+    console.log(response.body);
+    expect(response.status).toBe(200);
+  });
+
+  it("Should return 200 if the user is admin", async () => {
+    const response = await request(app)
+      .get("/campaigns/1/observations")
+      .set("Authorization", "Bearer admin");
+    console.log(response.body);
+    expect(response.status).toBe(200);
+  });
+});
---
+++ src/routes/campaigns/campaignId/observations/_get/index.ts
@@ -0,0 +1,26 @@
+/** OPENAPI-CLASS: get-campaigns-campaign-observations */
+
+import { tryber } from "@src/features/database";
+import OpenapiError from "@src/features/OpenapiError";
+import CampaignRoute from "@src/features/routes/CampaignRoute";
+export default class SingleCampaignRoute extends CampaignRoute<{
+  response: StoplightOperations["get-campaigns-campaign-observations"]["responses"]["200"]["content"]["application/json"];
+  parameters: StoplightOperations["get-campaigns-campaign-observations"]["parameters"]["path"];
+}> {
+  protected async filter(): Promise<boolean> {
+    if (!(await super.filter())) return false;
+
+    if (!this.hasAccessToCampaign(this.cp_id)) {
+      this.setError(403, new OpenapiError("Access denied"));
+      return false;
+    }
+
+    return true;
+  }
+
+  protected async prepare(): Promise<void> {
+    return this.setSuccess(200, {
+      items: [],
+    });
+  }
+}
