d781feae9fccf7c82e75e980d77335dfaee005d1
Gianpaolo Sinatra <gianpaolo.sinatra@app-quality.com>
2023-07-25 17:06:34 +0200

    feat(patch-ux): CASE publish first time a draft

--- src/routes/campaigns/campaignId/ux/_patch/index.spec.ts
+++ src/routes/campaigns/campaignId/ux/_patch/index.spec.ts
@@ -18,6 +18,22 @@ const requestBody = {
   insights: [],
   sentiments: [],
 };
+const singleInsight = {
+  title: "Insight Title",
+  description: "Insight Description",
+  severityId: 1,
+  order: 2,
+  clusterIds: "all",
+  videoPart: [],
+};
+const singleVideoPart = {
+  start: 6,
+  end: 9,
+  mediaId: 666,
+  description: "Videopart Description",
+  order: 9,
+};
+
 describe("PATCH /campaigns/{campaignId}/ux - permissions and loggin statuses", () => {
   beforeAll(async () => {
     await tryber.tables.WpAppqEvdCampaign.do().insert([{ ...campaign, id: 1 }]);
@@ -68,7 +84,13 @@ describe("PATCH /campaigns/{campaignId}/ux - CASE: publish first time a draft", 
   afterAll(async () => {
     await tryber.tables.WpAppqEvdCampaign.do().delete();
     await tryber.tables.UxCampaignData.do().delete();
+    await tryber.tables.UxCampaignInsights.do().delete();
   });
+  afterEach(async () => {
+    await tryber.tables.UxCampaignInsights.do().delete();
+    await tryber.tables.UxCampaignData.do().delete();
+  });
+
   it("should return 200", async () => {
     const response = await request(app)
       .patch("/campaigns/99/ux")
@@ -76,30 +98,139 @@ describe("PATCH /campaigns/{campaignId}/ux - CASE: publish first time a draft", 
       .set("Authorization", "Bearer admin");
     expect(response.status).toBe(200);
   });
-  //
+
   it("Should insert a new insight", async () => {
     const response = await request(app)
       .patch("/campaigns/99/ux")
       .send({
         ...requestBody,
-        insights: [
-          {
-            id: 1,
-            title: "test",
-            description: "test",
-            severityId: 1,
-            order: 1,
-            clusterId: "all",
-            videoPart: [],
-          },
-        ],
+        insights: [singleInsight],
       })
       .set("Authorization", "Bearer admin");
-    console.log(response.body);
-    const insight = await tryber.tables.UxCampaignData.do().select().first();
-    expect(insight).not.toBeUndefined();
-    console.log(insight);
-
+    const insighData = await tryber.tables.UxCampaignInsights.do()
+      .select()
+      .first();
+    expect(insighData).toBeDefined();
     expect(response.status).toBe(200);
+  });
+
+  it("Should insert a new insight with title", async () => {
+    await request(app)
+      .patch("/campaigns/99/ux")
+      .send({
+        ...requestBody,
+        insights: [singleInsight],
+      })
+      .set("Authorization", "Bearer admin");
+    const insights = await tryber.tables.UxCampaignInsights.do().select(
+      "title"
+    );
+    expect(insights).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          title: "Insight Title",
+        }),
+      ])
+    );
+  });
+
+  it("Should insert a new insight with description", async () => {
+    await request(app)
+      .patch("/campaigns/99/ux")
+      .send({
+        ...requestBody,
+        insights: [singleInsight],
+      })
+      .set("Authorization", "Bearer admin");
+    const insights = await tryber.tables.UxCampaignInsights.do().select(
+      "description"
+    );
+    expect(insights).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          description: "Insight Description",
+        }),
+      ])
+    );
+  });
+
+  it("Should insert a new insight with severityId", async () => {
+    await request(app)
+      .patch("/campaigns/99/ux")
+      .send({
+        ...requestBody,
+        insights: [singleInsight],
+      })
+      .set("Authorization", "Bearer admin");
+    const insights = await tryber.tables.UxCampaignInsights.do().select(
+      "severity_id"
+    );
+    expect(insights).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          severity_id: 1,
+        }),
+      ])
+    );
+  });
+
+  it("Should insert a new insight with order", async () => {
+    await request(app)
+      .patch("/campaigns/99/ux")
+      .send({
+        ...requestBody,
+        insights: [singleInsight],
+      })
+      .set("Authorization", "Bearer admin");
+    const insights = await tryber.tables.UxCampaignInsights.do().select(
+      "order"
+    );
+    expect(insights).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          order: 2,
+        }),
+      ])
+    );
+  });
+
+  it("Should insert a new insight with clusterIds all", async () => {
+    await request(app)
+      .patch("/campaigns/99/ux")
+      .send({
+        ...requestBody,
+        insights: [singleInsight],
+      })
+      .set("Authorization", "Bearer admin");
+    const insights = await tryber.tables.UxCampaignInsights.do().select(
+      "cluster_ids"
+    );
+    expect(insights).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          cluster_ids: "0",
+        }),
+      ])
+    );
+  });
+
+  it("Should insert a new insight with 2 clusterIds", async () => {
+    await request(app)
+      .patch("/campaigns/99/ux")
+      .send({
+        ...requestBody,
+        insights: [{ ...singleInsight, clusterIds: [3, 5] }],
+      })
+      .set("Authorization", "Bearer admin");
+    const insights = await tryber.tables.UxCampaignInsights.do().select(
+      "cluster_ids"
+    );
+    expect(insights).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          cluster_ids: "3,5",
+        }),
+      ])
+    );
   });
 });
--- src/routes/campaigns/campaignId/ux/_patch/index.ts
+++ src/routes/campaigns/campaignId/ux/_patch/index.ts
@@ -3,15 +3,15 @@
 import OpenapiError from "@src/features/OpenapiError";
 import { tryber } from "@src/features/database";
 import UserRoute from "@src/features/routes/UserRoute";
-//import UxData from "./UxData";
+import UxData from "./UxData";
 
 export default class PatchUx extends UserRoute<{
   response: StoplightOperations["patch-campaigns-campaign-ux"]["responses"]["200"]["content"]["application/json"];
   parameters: StoplightOperations["patch-campaigns-campaign-ux"]["parameters"]["path"];
-  request: StoplightOperations["patch-campaigns-campaign-ux"]["requestBody"]["content"]["application/json"];
+  body: StoplightOperations["patch-campaigns-campaign-ux"]["requestBody"]["content"]["application/json"];
 }> {
   private campaignId: number;
-  //private _draft: UxData | undefined;
+  private _draft: UxData | undefined;
 
   constructor(config: RouteClassConfiguration) {
     super(config);
@@ -65,12 +65,51 @@ export default class PatchUx extends UserRoute<{
   // }
 
   protected async prepare(): Promise<void> {
-    this.setSuccess(200, {
+    await this.fetchUx();
+    return this.setSuccess(200, {
       // status: await this.getStatus(),
       // insight: this.draft.data?.findings || [],
       // sentiments: [],
     });
   }
+
+  private async fetchUx() {
+    const requestBody = this.getBody();
+    const requestInsights = requestBody.insights ? requestBody.insights : [];
+
+    // insert insights
+    const mapInsightsToInsert = requestInsights.map((insight) => {
+      return {
+        campaign_id: this.campaignId,
+        version: 1,
+        title: insight.title ? insight.title : "",
+        description: insight.description ? insight.description : "",
+        severity_id: insight.severityId ? insight.severityId : 1,
+        cluster_ids:
+          Array.isArray(insight.clusterIds) && insight.clusterIds.length
+            ? insight.clusterIds.join(",")
+            : insight.clusterIds === "all"
+            ? "0"
+            : "0",
+        order: insight.order ? insight.order : 1,
+      };
+    });
+    await tryber.tables.UxCampaignInsights.do().insert(mapInsightsToInsert);
+
+    // insert video parts
+
+    // if(requestInsights && requestInsights.videoPart.length)
+  }
+  /**
+   *    id: number;
+        campaign_id: number;
+        version: number;
+        title: string;
+        description: string;
+        severity_id: number;
+        cluster_ids: string;
+        order: number;
+   */
 
   // private async getStatus() {
   //   const published = new UxData(this.campaignId);
