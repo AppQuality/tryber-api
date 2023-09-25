952382cfe08befcb755d88c506f0c535de7f7e8f
Kariamos <minucci9292@gmail.com>
2023-09-25 12:30:17 +0200

    typo(get-users-me): removed comment

--- src/routes/users/me/_get/getProfessionData.ts
+++ src/routes/users/me/_get/getProfessionData.ts
@@ -2,15 +2,7 @@ import { tryber } from "@src/features/database";
 import * as db from "@src/features/db";
 
 export default async (id: string) => {
-  /*let sql = `SELECT e.id,e.display_name as name
-    FROM wp_appq_evd_profile p
-    JOIN wp_appq_employment e ON (e.id = p.employment_id)
-    WHERE wp_user_id = ?;
-  `;*/
-
   try {
-    //const data = await db.query(db.format(sql, [id]));
-
     const data = await tryber.tables.WpAppqEvdProfile.do()
       .select(
         tryber.ref("id").withSchema("wp_appq_employment"),
