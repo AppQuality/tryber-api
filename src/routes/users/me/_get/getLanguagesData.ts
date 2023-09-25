92ea28c90c05f07ccb76d621ae9d3335b3113a44
ZecD <dominik.zecoli@gmail.com>
2023-09-25 17:09:09 +0200

    refactor- get-users-me knexified getEducationData and getLanguages Data

--- src/routes/users/me/_get/getLanguagesData.ts
+++ src/routes/users/me/_get/getLanguagesData.ts
@@ -1,25 +1,33 @@
-import * as db from "@src/features/db";
+import { tryber } from "@src/features/database";
 
 export default async (id: string) => {
-  let sql = `SELECT l.display_name as language, l.id as id
-               FROM wp_appq_profile_has_lang pl
-                   JOIN wp_appq_evd_profile p
-               on pl.profile_id = p.id
-                   JOIN wp_appq_lang l ON (pl.language_id = l.id)
-               WHERE wp_user_id = ?;
-    ;
-    `;
+  const data = await tryber.tables.WpAppqProfileHasLang.do()
+    .select(
+      tryber.ref("display_name").withSchema("wp_appq_lang").as("language"),
+      tryber.ref("id").withSchema("wp_appq_lang")
+    )
+    .as("id")
+    .join(
+      "wp_appq_evd_profile",
+      "wp_appq_evd_profile.id",
+      "wp_appq_profile_has_lang.profile_id"
+    )
+    .join(
+      "wp_appq_lang",
+      "wp_appq_lang.id",
+      "wp_appq_profile_has_lang.language_id"
+    )
+    .where("wp_appq_evd_profile.wp_user_id", id);
 
-  return db
-    .query(db.format(sql, [id]))
-    .then((data) => {
-      if (!data.length) return Promise.reject(Error("Invalid language data"));
-      return {
-        languages: data.map((l: { id: string; language: string }) => ({
-          id: l.id,
-          name: l.language,
-        })),
-      };
-    })
-    .catch((e) => Promise.reject(Error(e)));
+  try {
+    if (!data.length) return Promise.reject(Error("Invalid language data"));
+    return {
+      languages: data.map((l: { id: number; language: string }) => ({
+        id: l.id,
+        name: l.language,
+      })),
+    };
+  } catch (e) {
+    return Promise.reject(e);
+  }
 };
