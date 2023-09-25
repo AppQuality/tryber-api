3e75c265ba34321935456f101df59e2f81820790
Kariamos <minucci9292@gmail.com>
2023-09-25 12:27:21 +0200

    refactor(users-me-get): kexified getApprovedBugsData and getApprovedCpData. Added relative tests.
    Co-authored-by: iDome89 <iDome89@users.noreply.github.com>

--- src/routes/users/me/_get/getApprovedBugsData.ts
+++ src/routes/users/me/_get/getApprovedBugsData.ts
@@ -1,20 +1,19 @@
 import * as db from "@src/features/db";
+import { tryber } from "@src/features/database";
 
 export default async (id: string) => {
-  let sql = `SELECT COUNT(id) AS approved_bugs
-        FROM wp_appq_evd_bug b
-        WHERE b.wp_user_id  = ?
-        AND b.status_id = 2`;
-  return db
-    .query(db.format(sql, [id]))
-    .then((data) => {
-      if (!data.length) return Promise.reject(Error("Invalid bugs data"));
-      return { approved_bugs: data[0].approved_bugs };
-    })
-    .catch((e) => {
-      if (process.env && process.env.NODE_ENV === "development") {
-        console.log(e);
-      }
-      return Promise.reject(Error(e));
-    });
+  try {
+    const data = await tryber.tables.WpAppqEvdBug.do()
+      .count("id")
+      .where("wp_user_id", id)
+      .andWhere("status_id", 2)
+      .first();
+
+    if (!data) return Promise.reject(Error("Invalid bugs data"));
+
+    return { approved_bugs: Number(Object.values(data)[0]) };
+  } catch (e) {
+    if (process.env && process.env.NODE_ENV === "development") console.log(e);
+    return Promise.reject(e);
+  }
 };
