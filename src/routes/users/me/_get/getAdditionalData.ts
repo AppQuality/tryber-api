b2eac3b3d0d94ac47e7ecda68f649950656d151f
Gianpaolo Sinatra <gianpaolo.sinatra@app-quality.com>
2023-10-03 09:22:52 +0200

    refactor(get-users-me): use knex on getAdditionalData.ts

--- src/routes/users/me/_get/getAdditionalData.ts
+++ src/routes/users/me/_get/getAdditionalData.ts
@@ -1,18 +1,6 @@
-import * as db from "@src/features/db";
+import { tryber } from "@src/features/database";
 
 export default async (id: string, fieldId: false | string = false) => {
-  let sql = `SELECT cud.id, cuf.id as field_id,cuf.name, cuf.type, cud.value
-FROM wp_appq_custom_user_field_data cud
-         JOIN wp_appq_custom_user_field cuf ON (cuf.id = cud.custom_user_field_id)
-         JOIN wp_appq_evd_profile p ON (p.id = cud.profile_id)
-WHERE wp_user_id = ?
-  AND cuf.enabled = 1`;
-
-  let sqlData = [id];
-  if (fieldId) {
-    sql += " AND cuf.id = ?";
-    sqlData.push(parseInt(fieldId).toString());
-  }
   let data: {
     id: string;
     field_id: number;
@@ -26,7 +14,30 @@ WHERE wp_user_id = ?
 
   let additional: typeof data = [];
   try {
-    data = await db.query(db.format(sql, sqlData));
+    let newData = tryber.tables.WpAppqCustomUserFieldData.do()
+      .select(
+        tryber.ref("id").withSchema("wp_appq_custom_user_field_data"),
+        tryber.ref("id").withSchema("wp_appq_custom_user_field").as("field_id"),
+        tryber.ref("name").withSchema("wp_appq_custom_user_field"),
+        tryber.ref("type").withSchema("wp_appq_custom_user_field"),
+        tryber.ref("value").withSchema("wp_appq_custom_user_field_data")
+      )
+      .join(
+        "wp_appq_custom_user_field",
+        "wp_appq_custom_user_field.id",
+        "wp_appq_custom_user_field_data.custom_user_field_id"
+      )
+      .join(
+        "wp_appq_evd_profile",
+        "wp_appq_evd_profile.id",
+        "wp_appq_custom_user_field_data.profile_id"
+      )
+      .where("wp_user_id", id)
+      .andWhere("wp_appq_custom_user_field.enabled", 1);
+    if (fieldId) {
+      newData = newData.andWhere("wp_appq_custom_user_field.id", fieldId);
+    }
+    data = (await newData) as unknown as typeof data;
     if (!data.length)
       return Promise.reject({
         status_code: 404,
@@ -46,21 +57,37 @@ WHERE wp_user_id = ?
   });
 
   let selects: typeof data = [];
+
   const selectsIds = data
     .filter((i) => ["select", "multiselect"].includes(i.type))
-    .map((i) => parseInt(i.id))
-    .join(",");
+    .map((i) => parseInt(i.id));
 
   if (selectsIds.length) {
-    let selectsSql = `SELECT cud.id, COALESCE(cue.name, cud.value) as name, COALESCE(cue.id, cud.value) as value, cud.candidate
-      FROM wp_appq_custom_user_field_data cud
-      LEFT join wp_appq_custom_user_field_extras cue ON cud.value = cue.id
-      WHERE cud.id IN (${selectsIds}) AND (cud.candidate = 1 OR cue.name IS NOT NULL);`;
+    const newSelects = await tryber.tables.WpAppqCustomUserFieldData.do()
+      .select(
+        tryber.ref("id").withSchema("wp_appq_custom_user_field_data"),
+        tryber.raw(
+          "COALESCE(wp_appq_custom_user_field_extras.name, wp_appq_custom_user_field_data.value) as name"
+        ),
+        tryber.raw(
+          "COALESCE(wp_appq_custom_user_field_extras.id, wp_appq_custom_user_field_data.value) as value"
+        ),
+        tryber.ref("candidate").withSchema("wp_appq_custom_user_field_data")
+      )
+      .leftJoin(
+        "wp_appq_custom_user_field_extras",
+        "wp_appq_custom_user_field_data.value",
+        "wp_appq_custom_user_field_extras.id"
+      )
+      .whereIn("wp_appq_custom_user_field_data.id", selectsIds)
+      .andWhere(function () {
+        this.where("wp_appq_custom_user_field_data.candidate", 1),
+          this.orWhereNotNull("wp_appq_custom_user_field_extras.name");
+      });
 
     try {
-      const selectsData: typeof data = await db.query(
-        db.format(selectsSql, [id])
-      );
+      const selectsData: typeof data = newSelects as unknown as typeof data;
+
       selectsData.forEach((s) => {
         let select = data.find((d) => d.id === s.id);
         if (select) {
