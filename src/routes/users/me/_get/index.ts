585e8a0656fb05c524858dbc6242e7c3399ad841
Gianpaolo Sinatra <gianpaolo.sinatra@app-quality.com>
2023-10-10 12:36:34 +0200

    refactor(get-users-me): use updateLastActivity in class

--- src/routes/users/me/_get/index.ts
+++ src/routes/users/me/_get/index.ts
@@ -1,9 +1,9 @@
 /**  OPENAPI-CLASS : get-users-me */
 
 import UserRoute from "@src/features/routes/UserRoute";
-import updateLastActivity from "./updateLastActivity";
 import getUserData from "./getUserData";
 import debugMessage from "@src/features/debugMessage";
+import { tryber } from "@src/features/database";
 
 export default class UsersMe extends UserRoute<{
   response: StoplightOperations["get-users-me"]["responses"]["200"]["content"]["application/json"];
@@ -12,9 +12,8 @@ export default class UsersMe extends UserRoute<{
   protected async prepare() {
     let query = this.getQuery();
     let fields = query.fields ? query.fields.split(",") : false;
-    try {
-      await updateLastActivity(this.getTesterId());
-    } catch (e) {}
+
+    await this.updateLastActivity();
 
     try {
       const user = await getUserData(this.getWordpressId().toString(), fields);
@@ -22,6 +21,20 @@ export default class UsersMe extends UserRoute<{
         ...user,
         role: this.configuration.request.user.role,
       });
+    } catch (err) {
+      debugMessage(err);
+      this.setError(
+        (err as OpenapiError).status_code || 404,
+        err as OpenapiError
+      );
+    }
+  }
+
+  protected async updateLastActivity() {
+    try {
+      await tryber.tables.WpAppqEvdProfile.do()
+        .update({ last_activity: tryber.fn.now() })
+        .where({ id: this.getTesterId() });
     } catch (err) {
       debugMessage(err);
       this.setError(
