a257bb4263dbd78fa5b7004277f8edb9d3685cdf
Davide Bizzi <davide.bizzi@unguess.io>
2024-01-30 15:28:34 +0100

    feat: Map to distribution

--- src/routes/campaigns/campaignId/ux/UxData.ts
+++ src/routes/campaigns/campaignId/ux/UxData.ts
@@ -328,10 +328,42 @@ export default class UxData {
       const isValidPoster = await checkUrl(poster);
       video.push({
         ...v,
-        streamUrl: isValidStream ? stream : "",
-        poster: isValidPoster ? poster : undefined,
+        location: this.mapToDistribution(v.location),
+        streamUrl: isValidStream ? this.mapToDistribution(stream) : "",
+        poster: isValidPoster ? this.mapToDistribution(poster) : undefined,
       });
     }
     return video;
+  }
+
+  private mapToDistribution(url: string) {
+    if (url.includes("mediaconvert-encoder-staging-bucket")) {
+      return url.replace(
+        "https://s3-eu-west-1.amazonaws.com/mediaconvert-encoder-staging-bucket",
+        "https://media-processed.dev.tryber.me"
+      );
+    }
+    if (url.includes("mediaconvert-encoder-production-bucket")) {
+      return url.replace(
+        "https://s3-eu-west-1.amazonaws.com/mediaconvert-encoder-production-bucket",
+        "https://media-processed.tryber.me"
+      );
+    }
+
+    if (url.includes("mediaconvert-encoder-production-bucket-origin")) {
+      return url.replace(
+        "https://s3-eu-west-1.amazonaws.com/mediaconvert-encoder-production-bucket-origin",
+        "https://media-origin.tryber.me"
+      );
+    }
+
+    if (url.includes("mediaconvert-encoder-staging-bucket-origin")) {
+      return url.replace(
+        "https://s3-eu-west-1.amazonaws.com/mediaconvert-encoder-staging-bucket-origin",
+        "https://media-origin.dev.tryber.me"
+      );
+    }
+
+    return url;
   }
 }
