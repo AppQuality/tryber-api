6305402ade53e2766e0436a5f9c1e5a3b348645d
d-beezee <59012086+d-beezee@users.noreply.github.com>
2024-04-10 16:14:28 +0200

    Add campaign bundle (#298)
    * feat: Add basic design for dossiers
    
    * feat: Add basic structure for campaigns dossier
    
    * feat: Add device list
    
    * rework: Split to methods
    
    * ci: Add implicit dependency
    
    * feat: Add put campaign
    
    * feat: Add get dossier
    
    * feat: Add customer name
    
    * feat: Allow setting csm

---
+++ src/routes/dossiers/_post/index.ts
@@ -0,0 +1,116 @@
+/** OPENAPI-CLASS: post-dossiers */
+
+import OpenapiError from "@src/features/OpenapiError";
+import { tryber } from "@src/features/database";
+import AdminRoute from "@src/features/routes/AdminRoute";
+
+export default class RouteItem extends AdminRoute<{
+  response: StoplightOperations["post-dossiers"]["responses"]["201"]["content"]["application/json"];
+  body: StoplightOperations["post-dossiers"]["requestBody"]["content"]["application/json"];
+}> {
+  protected async filter() {
+    if (!(await super.filter())) return false;
+    if (!(await this.projectExists())) {
+      this.setError(400, new OpenapiError("Project does not exist"));
+      return false;
+    }
+    if (!(await this.testTypeExists())) {
+      this.setError(400, new OpenapiError("Test type does not exist"));
+      return false;
+    }
+    if (!(await this.deviceExists())) {
+      this.setError(400, new OpenapiError("Invalid devices"));
+      return false;
+    }
+
+    return true;
+  }
+
+  private async projectExists(): Promise<boolean> {
+    const { project: projectId } = this.getBody();
+    const project = await tryber.tables.WpAppqProject.do()
+      .select()
+      .where({
+        id: projectId,
+      })
+      .first();
+    return !!project;
+  }
+
+  private async testTypeExists(): Promise<boolean> {
+    const { testType: testTypeId } = this.getBody();
+    const testType = await tryber.tables.WpAppqCampaignType.do()
+      .select()
+      .where({
+        id: testTypeId,
+      })
+      .first();
+    return !!testType;
+  }
+
+  private async deviceExists(): Promise<boolean> {
+    const { deviceList } = this.getBody();
+    const devices = await tryber.tables.WpAppqEvdPlatform.do()
+      .select()
+      .whereIn("id", deviceList);
+    return devices.length === deviceList.length;
+  }
+
+  protected async prepare(): Promise<void> {
+    try {
+      this.setSuccess(201, {
+        id: await this.createCampaign(),
+      });
+    } catch (e) {
+      this.setError(500, e as OpenapiError);
+    }
+  }
+
+  private async createCampaign() {
+    const { os, form_factor } = await this.getDevices();
+
+    const results = await tryber.tables.WpAppqEvdCampaign.do()
+      .insert({
+        title: this.getBody().title.tester,
+        platform_id: 0,
+        start_date: this.getBody().startDate,
+        end_date: this.getEndDate(),
+        page_preview_id: 0,
+        page_manual_id: 0,
+        customer_id: 0,
+        pm_id: this.getCsmId(),
+        project_id: this.getBody().project,
+        campaign_type_id: this.getBody().testType,
+        customer_title: this.getBody().title.customer,
+        os: os.join(","),
+        form_factor: form_factor.join(","),
+      })
+      .returning("id");
+
+    return results[0].id ?? results[0];
+  }
+
+  private getCsmId() {
+    const { csm } = this.getBody();
+    return csm ? csm : this.getTesterId();
+  }
+
+  private getEndDate() {
+    if (this.getBody().endDate) return this.getBody().endDate;
+
+    const startDate = new Date(this.getBody().startDate);
+    startDate.setDate(startDate.getDate() + 7);
+    return startDate.toISOString().replace(/\.\d+/, "");
+  }
+
+  private async getDevices() {
+    const devices = await tryber.tables.WpAppqEvdPlatform.do()
+      .select("id", "form_factor")
+      .whereIn("id", this.getBody().deviceList);
+
+    const os = devices.map((device) => device.id);
+    const form_factor = devices.map((device) => device.form_factor);
+
+    return { os, form_factor };
+  }
+}
---
+++ src/routes/dossiers/campaignId/_get/index.spec.ts
@@ -0,0 +1,199 @@
+import app from "@src/app";
+import { tryber } from "@src/features/database";
+import request from "supertest";
+
+describe("Route GET /dossiers/:id", () => {
+  beforeAll(async () => {
+    await tryber.tables.WpAppqCustomer.do().insert({
+      id: 1,
+      company: "Test Company",
+      pm_id: 1,
+    });
+    await tryber.tables.WpAppqProject.do().insert({
+      id: 1,
+      display_name: "Test Project",
+      customer_id: 1,
+      edited_by: 1,
+    });
+
+    await tryber.tables.WpAppqEvdProfile.do().insert({
+      id: 1,
+      wp_user_id: 1,
+      name: "Test",
+      surname: "CSM",
+      email: "",
+      education_id: 1,
+      employment_id: 1,
+    });
+
+    await tryber.tables.WpAppqCampaignType.do().insert({
+      id: 1,
+      name: "Test Type",
+      description: "Test Description",
+      category_id: 1,
+    });
+
+    await tryber.tables.WpAppqEvdPlatform.do().insert([
+      {
+        id: 1,
+        name: "Test Device",
+        form_factor: 0,
+        architecture: 1,
+      },
+    ]);
+
+    await tryber.tables.WpAppqEvdCampaign.do().insert({
+      id: 1,
+      project_id: 1,
+      campaign_type_id: 1,
+      title: "Test Campaign",
+      customer_title: "Test Customer Campaign",
+      start_date: "2019-08-24T14:15:22Z",
+      end_date: "2019-08-24T14:15:22Z",
+      platform_id: 0,
+      os: "1",
+      page_manual_id: 0,
+      page_preview_id: 0,
+      pm_id: 1,
+      customer_id: 0,
+    });
+  });
+
+  afterAll(async () => {
+    await tryber.tables.WpAppqCustomer.do().delete();
+    await tryber.tables.WpAppqProject.do().delete();
+    await tryber.tables.WpAppqCampaignType.do().delete();
+    await tryber.tables.WpAppqEvdPlatform.do().delete();
+    await tryber.tables.WpAppqEvdCampaign.do().delete();
+    await tryber.tables.WpAppqEvdProfile.do().delete();
+  });
+
+  it("Should answer 403 if not logged in", async () => {
+    const response = await request(app).get("/dossiers/1");
+    expect(response.status).toBe(403);
+  });
+
+  it("Should answer 403 if campaign does not exists", async () => {
+    const response = await request(app).get("/dossiers/10");
+    expect(response.status).toBe(403);
+  });
+
+  it("Should answer 403 if not admin", async () => {
+    const response = await request(app)
+      .get("/dossiers/1")
+      .set("authorization", "Bearer tester");
+    console.log(response.body);
+    expect(response.status).toBe(403);
+  });
+
+  it("Should answer 200 if admin", async () => {
+    const response = await request(app)
+      .get("/dossiers/1")
+      .set("authorization", "Bearer admin");
+    console.log(response.body);
+    expect(response.status).toBe(200);
+  });
+
+  it("Should return the campaign id", async () => {
+    const response = await request(app)
+      .get("/dossiers/1")
+      .set("authorization", "Bearer admin");
+    expect(response.status).toBe(200);
+
+    expect(response.body).toHaveProperty("id");
+    expect(response.body.id).toBe(1);
+  });
+  it("Should return the campaign tester title", async () => {
+    const response = await request(app)
+      .get("/dossiers/1")
+      .set("authorization", "Bearer admin");
+    expect(response.status).toBe(200);
+
+    expect(response.body).toHaveProperty("title");
+    expect(response.body.title).toHaveProperty("tester", "Test Campaign");
+  });
+
+  it("Should return the campaign customer title", async () => {
+    const response = await request(app)
+      .get("/dossiers/1")
+      .set("authorization", "Bearer admin");
+
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty("title");
+    expect(response.body.title).toHaveProperty(
+      "customer",
+      "Test Customer Campaign"
+    );
+  });
+
+  it("Should return the project", async () => {
+    const response = await request(app)
+      .get("/dossiers/1")
+      .set("authorization", "Bearer admin");
+
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty("project");
+    expect(response.body.project).toHaveProperty("id", 1);
+    expect(response.body.project).toHaveProperty("name", "Test Project");
+  });
+
+  it("Should return the test type", async () => {
+    const response = await request(app)
+      .get("/dossiers/1")
+      .set("authorization", "Bearer admin");
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty("testType");
+    expect(response.body.testType).toHaveProperty("id", 1);
+    expect(response.body.testType).toHaveProperty("name", "Test Type");
+  });
+
+  it("Should return the start date", async () => {
+    const response = await request(app)
+      .get("/dossiers/1")
+      .set("authorization", "Bearer admin");
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty("startDate", "2019-08-24T14:15:22Z");
+  });
+
+  it("Should return the end date", async () => {
+    const response = await request(app)
+      .get("/dossiers/1")
+      .set("authorization", "Bearer admin");
+
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty("endDate", "2019-08-24T14:15:22Z");
+  });
+
+  it("Should return the device list", async () => {
+    const response = await request(app)
+      .get("/dossiers/1")
+      .set("authorization", "Bearer admin");
+
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty("deviceList");
+    expect(response.body.deviceList).toHaveLength(1);
+    expect(response.body.deviceList[0]).toHaveProperty("id", 1);
+    expect(response.body.deviceList[0]).toHaveProperty("name", "Test Device");
+  });
+
+  it("Should return the csm", async () => {
+    const response = await request(app)
+      .get("/dossiers/1")
+      .set("authorization", "Bearer admin");
+
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty("csm");
+    expect(response.body.csm).toHaveProperty("id", 1);
+    expect(response.body.csm).toHaveProperty("name", "Test CSM");
+  });
+  it("Should return the customer", async () => {
+    const response = await request(app)
+      .get("/dossiers/1")
+      .set("authorization", "Bearer admin");
+
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty("customer");
+    expect(response.body.customer).toHaveProperty("id", 1);
+    expect(response.body.customer).toHaveProperty("name", "Test Company");
+  });
+});
