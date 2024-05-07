cc6d737bea4758737ab53dcd1aafd843b2e05813
d-beezee <59012086+d-beezee@users.noreply.github.com>
2024-05-07 15:30:53 +0200

    feat: Add phase type (#318)

--- src/routes/phases/_get/index.ts
+++ src/routes/phases/_get/index.ts
@@ -26,9 +26,24 @@ export default class Route extends UserRoute<{
     return this.accessibleCampaigns !== true;
   }
   protected async prepare(): Promise<void> {
-    const results = await tryber.tables.CampaignPhase.do().select("id", "name");
+    const results = await tryber.tables.CampaignPhase.do()
+      .join(
+        "campaign_phase_type",
+        "campaign_phase_type.id",
+        "campaign_phase.type_id"
+      )
+      .select(
+        tryber.ref("id").withSchema("campaign_phase"),
+        tryber.ref("name").withSchema("campaign_phase"),
+        tryber.ref("id").withSchema("campaign_phase_type").as("type_id"),
+        tryber.ref("name").withSchema("campaign_phase_type").as("type_name")
+      );
     this.setSuccess(200, {
-      results,
+      results: results.map((phase) => ({
+        id: phase.id,
+        name: phase.name,
+        type: { id: phase.type_id, name: phase.type_name },
+      })),
     });
   }
 }
