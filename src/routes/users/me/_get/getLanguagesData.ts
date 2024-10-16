afc1f43e73fbf49e32ecb114c915a8d9b6d3cd4c
Kariamos <minucci9292@gmail.com>
2024-10-16 12:40:39 +0200

    refactor: fixed tests and code to use new language_name column

--- src/routes/users/me/_get/getLanguagesData.ts
+++ src/routes/users/me/_get/getLanguagesData.ts
@@ -1,25 +1,12 @@
+import { tryber } from "@src/features/database";
 import * as db from "@src/features/db";
 
 export default async (id: string) => {
-  let sql = `SELECT l.display_name as language, l.id as id
-               FROM wp_appq_profile_has_lang pl
-                   JOIN wp_appq_evd_profile p
-               on pl.profile_id = p.id
-                   JOIN wp_appq_lang l ON (pl.language_id = l.id)
-               WHERE wp_user_id = ?;
-    ;
-    `;
+  const result = await tryber.tables.WpAppqProfileHasLang.do()
+    .select("language_name")
+    .where("profile_id", id);
 
-  return db
-    .query(db.format(sql, [id]))
-    .then((data) => {
-      if (!data.length) return Promise.reject(Error("Invalid language data"));
-      return {
-        languages: data.map((l: { id: string; language: string }) => ({
-          id: l.id,
-          name: l.language,
-        })),
-      };
-    })
-    .catch((e) => Promise.reject(Error(e)));
+  if (!result.length) return Promise.reject(Error("Invalid language data"));
+
+  return { languages: result.map((l) => ({ name: l.language_name })) };
 };
