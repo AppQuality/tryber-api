2e3574373e0d2b097660d05a36f7cc22e38a4693
Kariamos <minucci9292@gmail.com>
2024-10-17 18:40:01 +0200

    refactor: update query to use wp_user_id

--- src/routes/users/me/_get/getLanguagesData.ts
+++ src/routes/users/me/_get/getLanguagesData.ts
@@ -4,7 +4,12 @@ import * as db from "@src/features/db";
 export default async (id: string) => {
   const result = await tryber.tables.WpAppqProfileHasLang.do()
     .select("language_name")
-    .where("profile_id", id);
+    .join(
+      "wp_appq_evd_profile",
+      "wp_appq_profile_has_lang.profile_id",
+      "wp_appq_evd_profile.id"
+    )
+    .where("wp_appq_evd_profile.wp_user_id", id);
 
   if (!result.length) return Promise.reject(Error("Invalid language data"));
 
