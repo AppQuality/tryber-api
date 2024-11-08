72e2ebb694d75ebdc0a28f605d82c98f4fb6357d
Davide Bizzi <davide.bizzi@unguess.io>
2024-11-08 15:48:25 +0100

    rework: Use on duplicate key update for candidature

--- src/routes/campaigns/campaignId/candidates/_post/createCandidature/index.ts
+++ src/routes/campaigns/campaignId/candidates/_post/createCandidature/index.ts
@@ -5,23 +5,8 @@ export default async (
   campaignId: number,
   selectedDevice: number
 ) => {
-  const isCandidate = await tryber.tables.WpCrowdAppqHasCandidate.do()
-    .select()
-    .where("user_id", wpId)
-    .where("campaign_id", campaignId);
-
-  if (isCandidate.length) {
-    await tryber.tables.WpCrowdAppqHasCandidate.do()
-      .update({
-        accepted: 1,
-        selected_device: selectedDevice,
-        accepted_date: tryber.fn.now(),
-        results: 0,
-      })
-      .where("user_id", wpId)
-      .where("campaign_id", campaignId);
-  } else {
-    await tryber.tables.WpCrowdAppqHasCandidate.do().insert({
+  await tryber.tables.WpCrowdAppqHasCandidate.do()
+    .insert({
       user_id: wpId,
       campaign_id: campaignId,
       accepted: 1,
@@ -30,8 +15,15 @@ export default async (
       selected_device: selectedDevice,
       group_id: 1,
       accepted_date: tryber.fn.now(),
+    })
+    .onConflict(["user_id", "campaign_id"])
+    .merge({
+      accepted: 1,
+      selected_device: selectedDevice,
+      accepted_date: tryber.fn.now(),
+      results: 0,
     });
-  }
+
   return {
     wordpress_id: wpId,
     campaign_id: campaignId,
