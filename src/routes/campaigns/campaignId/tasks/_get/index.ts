230bfb9c7d5cbf9b31047d5b079d41229bdeb062
Gianpaolo <gianpaolosinatra@gmail.com>
2025-06-09 11:55:46 +0200

    feat(usecases): implement GetUsecasesRoute and corresponding tests for campaign tasks retrieval

--- src/routes/campaigns/campaignId/tasks/_get/index.ts
+++ src/routes/campaigns/campaignId/tasks/_get/index.ts
@@ -1,12 +1,12 @@
-/** OPENAPI-CLASS : get-campaigns-campaign-usecases */
+/** OPENAPI-CLASS : get-campaigns-campaign-tasks */
 
 import { tryber } from "@src/features/database";
 import OpenapiError from "@src/features/OpenapiError";
 import CampaignRoute from "@src/features/routes/CampaignRoute";
 
 class GetUsecasesRoute extends CampaignRoute<{
-  response: StoplightOperations["get-campaigns-campaign-usecases"]["responses"]["200"]["content"]["application/json"];
-  parameters: StoplightOperations["get-campaigns-campaign-usecases"]["parameters"]["path"];
+  response: StoplightOperations["get-campaigns-campaign-tasks"]["responses"]["200"]["content"]["application/json"];
+  parameters: StoplightOperations["get-campaigns-campaign-tasks"]["parameters"]["path"];
 }> {
   protected async filter() {
     if ((await super.filter()) === false) return false;
@@ -29,7 +29,18 @@ class GetUsecasesRoute extends CampaignRoute<{
   }
 
   private async getUsecases() {
-    return [];
+    const { campaign } = this.getParameters();
+    const usecases = await tryber.tables.WpAppqCampaignTask.do()
+      .select("id", "title", "content", "campaign_id")
+      .where("campaign_id", campaign);
+    if (!usecases || usecases.length < 1) return [];
+
+    return usecases.map((usecase) => ({
+      id: usecase.id,
+      name: usecase.title,
+      content: usecase.content,
+      campaign_id: usecase.campaign_id,
+    }));
   }
 }
 
