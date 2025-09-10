43a3e5ef0a3fc2f42ca09228dbecf747ec585150
Kariamos <minucci9292@gmail.com>
2025-09-10 17:11:09 +0200

    feat: update task handling in campaign tasks POST route tests

--- src/routes/users/me/campaigns/campaignId/tasks/taskId/_post/index.spec.ts
+++ src/routes/users/me/campaigns/campaignId/tasks/taskId/_post/index.spec.ts
@@ -3,22 +3,102 @@ import { tryber } from "@src/features/database";
 import request from "supertest";
 import useBasicData from "../../../_get/useBasicData";
 
-describe("Route GET /users/me/campaigns/{campaignId}/", () => {
+describe("Route POST /users/me/campaigns/{campaignId}/tasks/{taskId}", () => {
   useBasicData();
   it("Should return 403 if user is not logged in", async () => {
-    const res = await request(app).get("/users/me/campaigns/1");
+    const res = await request(app)
+      .post("/users/me/campaigns/1/tasks/1")
+      .send({ status: "completed" });
     expect(res.status).toBe(403);
   });
+
   it("Should return 404 if user is logged in but not selected", async () => {
     const res = await request(app)
-      .get("/users/me/campaigns/2")
+      .post("/users/me/campaigns/2/tasks/1")
+      .send({ status: "completed" })
       .set("Authorization", "Bearer tester");
     expect(res.status).toBe(404);
   });
+
+  it("Should return 403 if user is logged in but the task does not belong to the campaign", async () => {
+    const res = await request(app)
+      .post("/users/me/campaigns/1/tasks/999")
+      .send({ status: "completed" })
+      .set("Authorization", "Bearer tester");
+    expect(res.status).toBe(403);
+  });
   it("Should return 200 if user is logged and selected", async () => {
     const response = await request(app)
-      .get("/users/me/campaigns/1")
+      .post("/users/me/campaigns/1/tasks/1")
+      .send({ status: "completed" })
       .set("Authorization", "Bearer tester");
     expect(response.status).toBe(200);
+  });
+  describe("Check payload", () => {
+    beforeEach(async () => {
+      await tryber.tables.WpAppqUserTask.do().insert({
+        id: 1,
+        tester_id: 1,
+        task_id: 1,
+        is_completed: 0,
+      });
+    });
+    afterEach(async () => {
+      await tryber.tables.WpAppqUserTask.do().delete();
+    });
+    describe("Invalid body", () => {
+      it("Should return 400 if status is missing", async () => {
+        const response = await request(app)
+          .post("/users/me/campaigns/1/tasks/1")
+          .send({})
+          .set("Authorization", "Bearer tester");
+        expect(response.status).toBe(400);
+      });
+
+      it("Should return 400 if the body is missing", async () => {
+        const response = await request(app)
+          .post("/users/me/campaigns/1/tasks/1")
+          .set("Authorization", " Bearer tester");
+        expect(response.status).toBe(400);
+      });
+
+      it("Should return 400 if status is not valid", async () => {
+        const response = await request(app)
+          .post("/users/me/campaigns/1/tasks/1")
+          .send({ status: "invalid_status" })
+          .set("Authorization", " Bearer tester");
+        expect(response.status).toBe(400);
+      });
+
+      it("Should return 400 if payload has incorrect fields", async () => {
+        const response = await request(app)
+          .post("/users/me/campaigns/1/tasks/1")
+          .send({ wrong_field: "completed" })
+          .set("Authorization", " Bearer tester");
+        expect(response.status).toBe(400);
+      });
+    });
+
+    describe("Valid body", () => {
+      it("Should return 200 if payload is valid", async () => {
+        const valueBefore = await tryber.tables.WpAppqUserTask.do()
+          .select("is_completed")
+          .where({ id: 1 })
+          .first();
+        expect(valueBefore?.is_completed).toBe(0);
+
+        const response = await request(app)
+          .post("/users/me/campaigns/1/tasks/1")
+          .send({ status: "completed" })
+          .set("Authorization", " Bearer tester");
+        expect(response.status).toBe(200);
+
+        const valueAfter = await tryber.tables.WpAppqUserTask.do()
+          .select("is_completed")
+          .where({ id: 1 })
+          .first();
+        expect(valueAfter?.is_completed).toBe(1);
+      });
+    });
   });
 });
