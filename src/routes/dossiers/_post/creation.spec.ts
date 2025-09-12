fb59292d9892791a16b755efd3451936750ccebe
Gianpaolo <gianpaolosinatra@gmail.com>
2025-09-12 17:50:20 +0200

    refactor: Remove console log from campaign creation test

--- src/routes/dossiers/_post/creation.spec.ts
+++ src/routes/dossiers/_post/creation.spec.ts
@@ -126,7 +126,6 @@ describe("Route POST /dossiers", () => {
       .post("/dossiers")
       .set("authorization", "Bearer admin")
       .send(baseRequest);
-    console.log(postResponse.body);
     expect(postResponse.status).toBe(201);
     expect(postResponse.body).toHaveProperty("id");
 
