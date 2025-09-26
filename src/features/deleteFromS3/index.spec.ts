5277a8ccbed319b6d8903432b76e1d54b91f9230
Davide Bizzi <davide.bizzi@unguess.io>
2025-09-26 14:32:58 +0200

    refactor: Simplify decomposeS3Url function and update tests for consistency

--- src/features/deleteFromS3/index.spec.ts
+++ src/features/deleteFromS3/index.spec.ts
@@ -4,19 +4,20 @@ import { decomposeS3Url } from ".";
 describe("Decompose s3 url", () => {
   it("Should return bucket and key", () => {
     const url =
-      "https://s3.eu-west-1.amazonaws.com/media.bucket/media/T11205/txt 1653662342424.json";
+      "https://s3-eu-west-1.amazonaws.com/mediaconvert.bucket/CP7634/UC41048/T11205/3ff2c0c2f7a2084afe1e0c5c7541b3dabfb0c0cf1df2e71b593e07ed7f260808.mp4";
     const { bucket, key } = decomposeS3Url(url);
-    expect(bucket).toBe("media.bucket");
-    expect(key).toBe("media/T11205/txt 1653662342424.json");
-  });
-
-  it("Should return empty bucket and key for invalid url", () => {
-    const url =
-      "https://mediaconvert-encoder-staging-bucket-origin.s3.eu-west-1.amazonaws.com/CP7634/UC41048/T11205/6055f5d549030c58337ec5fc119c6b59cdf4576fb36eb7911336309e710a1deb.mp4";
-    const { bucket, key } = decomposeS3Url(url);
-    expect(bucket).toBe("mediaconvert-encoder-staging-bucket-origin");
+    expect(bucket).toBe("mediaconvert.bucket");
     expect(key).toBe(
-      "CP7634/UC41048/T11205/6055f5d549030c58337ec5fc119c6b59cdf4576fb36eb7911336309e710a1deb.mp4"
+      "CP7634/UC41048/T11205/3ff2c0c2f7a2084afe1e0c5c7541b3dabfb0c0cf1df2e71b593e07ed7f260808.mp4"
+    );
+  });
+  it("Should return bucket and key", () => {
+    const url =
+      "https://s3.eu-west-1.amazonaws.com/mediaconvert.bucket/CP7634/UC41048/T11205/3ff2c0c2f7a2084afe1e0c5c7541b3dabfb0c0cf1df2e71b593e07ed7f260808.mp4";
+    const { bucket, key } = decomposeS3Url(url);
+    expect(bucket).toBe("mediaconvert.bucket");
+    expect(key).toBe(
+      "CP7634/UC41048/T11205/3ff2c0c2f7a2084afe1e0c5c7541b3dabfb0c0cf1df2e71b593e07ed7f260808.mp4"
     );
   });
 });
