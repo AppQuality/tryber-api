c9de03090150941fae72ec296c0c22f8ddfb03ec
Kariamos <minucci9292@gmail.com>
2026-01-28 12:02:35 +0100

    feat: implement finance suppliers endpoint for campaigns

---
+++ src/routes/campaigns/campaignId/finance/supplier/_get/index.ts
@@ -0,0 +1,45 @@
+/** OPENAPI-CLASS: get-campaigns-campaign-finance-supplier  */
+
+import CampaignRoute from "@src/features/routes/CampaignRoute";
+import { tryber } from "@src/features/database";
+import OpenapiError from "@src/features/OpenapiError";
+
+type Supplier = {
+  id: number;
+  name: string;
+  created_on?: string;
+  created_by?: number;
+};
+
+export default class SupplierRoute extends CampaignRoute<{
+  response: StoplightOperations["get-campaigns-campaign-finance-supplier"]["responses"]["200"]["content"]["application/json"];
+  parameters: StoplightOperations["get-campaigns-campaign-finance-supplier"]["parameters"]["path"];
+}> {
+  protected async filter(): Promise<boolean> {
+    if (!(await super.filter())) return false;
+
+    if (!this.hasAccessToCampaign(this.cp_id)) {
+      this.setError(403, new OpenapiError("Access denied"));
+
+      return false;
+    }
+    return true;
+  }
+
+  protected async prepare(): Promise<void> {
+    const suppliers = await this.getSuppliers();
+
+    return this.setSuccess(200, { items: suppliers });
+  }
+
+  private async getSuppliers(): Promise<Supplier[]> {
+    const results =
+      await tryber.tables.WpAppqCampaignOtherCostsSupplier.do().select(
+        "id",
+        "name",
+        "created_on",
+        "created_by"
+      );
+    return results;
+  }
+}
